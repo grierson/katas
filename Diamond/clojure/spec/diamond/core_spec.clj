f06690a6f32962859fcc337302da95c12262a1fd
Kyle Grierson <kyle.grierson@knowsley.gov.uk>
2017-08-25 10:32:37 +0100

    rm old Bowlling and Diamond

--- Diamond/clojure/spec/diamond/core_spec.clj
+++
@@ -1,8 +0,0 @@
-(ns diamond.core-spec
-  (:require [clojure.spec.alpha :as s]
-            [clojure.spec.gen.alpha :as gen]
-            [clojure.spec.test.alpha :as st]))
-
-(s/def ::letter (s/with-gen #(Character/isUpperCase %) gen/char-alpha))
-
-(s/exercise ::letter)
