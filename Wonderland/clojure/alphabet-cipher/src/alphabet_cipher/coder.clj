a336314c7dd22021fa395465da5778cf31f64ccf
Kyle Grierson <grierson@openmailbox.org>
2017-09-11 23:22:32 +0100

    Encode message

--- Wonderland/clojure/alphabet-cipher/src/alphabet_cipher/coder.clj
+++ Wonderland/clojure/alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,10 +1,29 @@
-(ns alphabet-cipher.coder)
+(ns alphabet-cipher.coder
+  (:require [clojure.spec.alpha :as s]
+            [clojure.spec.test.alpha :as st]))
+
+
+(def alphabet "abcdefghijklmnopqrstuvwxyz")
+
+
+(defn value [letter]
+  (.indexOf alphabet letter))
+
+
+(defn substitution
+  "Keyword char + Message char = encoded char"
+  [keyword message]
+  (nth alphabet (rem (+ (value (str keyword)) (value (str message))) 26)))
+
 
 (defn encode [keyword message]
-  "encodeme")
+  (let [cipher (take (count message) (cycle keyword))]
+    (apply str (map substitution cipher message))))
+
 
 (defn decode [keyword message]
   "decodeme")
+
 
 (defn decipher [cipher message]
   "decypherme")
