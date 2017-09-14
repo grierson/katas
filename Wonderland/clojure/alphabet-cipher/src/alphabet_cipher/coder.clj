38383b9f80a4aa182ad78abb701b0c5d663e5e2c
Kyle Grierson <kyle.grierson@knowsley.gov.uk>
2017-09-14 11:41:19 +0100

    Fixed Alphabet cipher

--- Wonderland/clojure/alphabet-cipher/src/alphabet_cipher/coder.clj
+++ Wonderland/clojure/alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -16,42 +16,14 @@
   (nth alphabet (rem (+ (value (str keyword)) (value (str message))) 26)))
 
 
-
-;; Domain
-
-(def alphabet (\a \b \c \d \e \f \g \h \i \j \k \l \m \n \o \p \q \r \s \t \u \v \w \x \y \z))
-
-
-(defn pos [x]
-  (- (int x) 97))
-
-
-(defn offset [letter]
-  (mod (pos letter) (count alphabet)))
-
-(offset \a)
-
-;; (letter from keyword) + (letter from message) = (encoded letter)
-
-;; Logic
-;; (offset "s" "m")
-;; e
-
-
 (defn encode [keyword message]
-<<<<<<< HEAD
-  "egsgqwtahuiljgs")
-=======
   (let [cipher (take (count message) (cycle keyword))]
     (apply str (map substitution cipher message))))
->>>>>>> a336314c7dd22021fa395465da5778cf31f64ccf
 
 
 (defn decode [keyword message]
-  "meetmeontuesdayeveningatseven")
-
+  "decodeme")
 
 
 (defn decipher [cipher message]
-  "vigilance")
-
+  "decypherme")
