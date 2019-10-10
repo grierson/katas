75a88feccade96dbf7c843be200acf4a127fcc4d
kyleg <griersonky@gmail.com>
2019-10-10 16:04:46 +0100

    Get all next words

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -1,13 +1,29 @@
 (ns doublets.solver
-  (:require [clojure.java.io :as io]
-            [clojure.edn :as edn]))
+  (:require [clojure.java.io :as io]))
 
+;; Filter dictionary
+;; Find all words within one letter
 
 (def words (-> "words.edn"
                (io/resource)
                (slurp)
                (read-string)))
 
+(defn same-length-words [dictionary length]
+  (filter (fn [word] (= length (count word))) dictionary))
+
+(defn compare-words [word1 word2]
+  (= 1 (count (filter false? (map (fn [a b] (= a b)) word1 word2)))))
+
+(defn within-one-letter [dictionary word]
+  (filter #(compare-words word %) dictionary))
 
 (defn doublets [word1 word2]
-  "make me work")
+ (let [dictionary (same-length-words words (count word1))
+       state #{word1}
+       next-words (within-one-letter dictionary (last state))]
+   next-words))
+
+(comment
+  (def cheat (same-length-words words (count "door")))
+  (within-one-letter cheat "lock"))
