4bfb5f9c7ab0eea5a4713e3b8147fe9e973b960b
kyleg <griersonky@gmail.com>
2019-10-11 16:18:01 +0100

    Added runner

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -18,12 +18,24 @@
 (defn within-one-letter [dictionary word]
   (filter #(compare-words word %) dictionary))
 
+(defn runner [dictionary state goal]
+  (let [next-words (within-one-letter dictionary (last state))
+        possible-next-words (set (filter #(contains? state %) next-words))]
+    (if (contains? possible-next-words goal)
+      (conj state goal)
+      (cond
+        (= 1 (count possible-next-words)) (runner dictionary (conj state (first possible-next-words)) goal)
+        :else state))))
+
 (defn doublets [word1 word2]
  (let [dictionary (same-length-words words (count word1))
-       state #{word1}
-       next-words (within-one-letter dictionary (last state))]
-   next-words))
+       state #{word1}]
+   (runner dictionary state word2))
 
 (comment
   (def cheat (same-length-words words (count "door")))
-  (within-one-letter cheat "lock"))
+  (within-one-letter cheat "door")
+  (runner cheat #{"door"} "lock")
+  (within-one-letter cheat "boor")
+  (within-one-letter cheat "lock")
+  (within-one-letter cheat "look"))
