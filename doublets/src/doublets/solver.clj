ba5282ff001abf5df62e050bb72dda2af7a77e37
kyleg <griersonky@gmail.com>
2019-10-16 10:01:54 +0100

    Done

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -3,40 +3,47 @@
 
 ;; Filter dictionary
 ;; Find all words within one letter
+;; Search each possibility
+;; Get shortest
 
 (def words (-> "words.edn"
                (io/resource)
                (slurp)
                (read-string)))
 
+
 (defn same-length-words [dictionary length]
-  (filter (fn [word] (= length (count word))) dictionary))
+  (filter #(= (count %) length) dictionary))
+
 
 (defn compare-words [word1 word2]
-  (= 1 (count (filter false? (map (fn [a b] (= a b)) word1 word2)))))
+  (= 1 (count (filter false? (map = word1 word2)))))
+
 
 (defn within-one-letter [dictionary word]
   (filter #(compare-words word %) dictionary))
 
-(comment
-  (def dictionary (same-length-words words (count "door")))
-  (def state ["door" "boor" "book"])
-  (def next-words '("boor" "look" "bonk"))
-  (def possible-next-words '("look" "bonk"))
-  (def goal "lock"))
 
-;; How to handle multiple possible-next-words?
+(defn contains-word [words word]
+  (some #(= word %) words))
+
 
 (defn runner [dictionary state goal]
   (let [next-words (within-one-letter dictionary (last state))
-        possible-next-words (remove (fn [word] (some #(= word %) state)) next-words)]
-    (if (some #(= goal %) possible-next-words)
+        possible-next-words (remove #(contains-word state %) next-words)]
+    (if (contains-word possible-next-words goal)
       (conj state goal)
-      (cond
-        (= 1 (count possible-next-words)) (runner dictionary (conj state (first possible-next-words)) goal)
-        :else state))))
+      (map #(flatten (runner dictionary (conj state %) goal)) possible-next-words))))
+
 
 (defn doublets [word1 word2]
- (let [dictionary (same-length-words words (count word1))
-       state #{word1}]
-   (runner dictionary state word2)))
+  (if (not= (count word1) (count word2))
+    []
+    (let [dictionary (same-length-words words (count word1))
+          state [word1]]
+      (first (runner dictionary state word2)))))
+
+(comment
+  (def cheat (same-length-words words (count "wheat")))
+  (runner cheat ["head"] "tail")
+  (runner cheat ["wheat"] "bread"))
