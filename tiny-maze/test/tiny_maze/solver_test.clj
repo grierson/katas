1189254549deb778e771e6df5586d7b90b593b28
kyleg <griersonky@gmail.com>
2019-10-16 17:27:23 +0100

    formatted code and change to Clojure 1.10

--- tiny-maze/test/tiny_maze/solver_test.clj
+++ tiny-maze/test/tiny_maze/solver_test.clj
@@ -5,28 +5,28 @@
 (deftest test-solve-maze
   (testing "can find way to exit with 3x3 maze"
     (let [maze [[:S 0 1]
-                [1  0 1]
-                [1  0 :E]]
+                [1 0 1]
+                [1 0 :E]]
           sol [[:x :x 1]
-               [1  :x 1]
-               [1  :x :x]]]
+               [1 :x 1]
+               [1 :x :x]]]
       (is (= sol (solve-maze maze)))))
 
-    (testing "can find way to exit with 4x4 maze"
+  (testing "can find way to exit with 4x4 maze"
     (let [maze [[:S 0 0 1]
-                [1  1 0 0]
-                [1  0  0 1]
-                [1  1  0 :E]]
+                [1 1 0 0]
+                [1 0 0 1]
+                [1 1 0 :E]]
           sol [[:x :x :x 1]
-                [1  1 :x 0]
-                [1  0 :x 1]
-                [1  1  :x :x]]]
-     (is (= sol (solve-maze maze))))))
+               [1 1 :x 0]
+               [1 0 :x 1]
+               [1 1 :x :x]]]
+      (is (= sol (solve-maze maze))))))
 
 
 (def sample-maze [[:S 0 1]
-           [1  0 1]
-           [1  0 :E]])
+                  [1 0 1]
+                  [1 0 :E]])
 
 (deftest test-free-space
   (testing "can find start space"
