639c07b478d912b9af1a01773ec67c64bdf19779
kyleg <griersonky@gmail.com>
2019-11-05 16:29:41 +0000

    Added extras tests

--- Bowling/clojure/bowling/src/bowling/core.clj
+++ Bowling/clojure/bowling/src/bowling/core.clj
@@ -16,23 +16,19 @@
 
 (defn str->value [[throw1 throw2 throw3 :as frame]]
   (cond
-    (strike? frame) [\X]
     (some? throw3) [(char->value throw1) (char->value throw2) (char->value throw3)]
+    (strike? frame) [\X]
     :else [(char->value throw1) (char->value throw2)]))
 
 (defn line->data [line]
   (let [frames (str/split line #" ")]
-    (if (= (count frames) 12)
-      (seq (conj (vec (map str->value (drop 3 frames))) (vec (mapcat str->value (take-last 3 frames)))))
-      (map str->value frames))))
+    (map str->value frames)))
 
 (defn sum-frame [[throw1 throw2]]
   (+ throw1 throw2))
 
 (defn score-ball [ball]
-  (if (= ball \X)
-    10
-    ball))
+  (if (= ball \X) 10 ball))
 
 (defn score-last-frame [[_ throw2 throw3 :as frame]]
   (cond
@@ -40,28 +36,27 @@
     (spare? frame) (+ 10 (score-ball throw3))
     :else (sum-frame frame)))
 
-(defn score-strike [line index]
-  (let [frame (nth line index [0 0])
-        next-frame (nth line (+ index 1) [0 0])]
-    (cond
-      (= (count frame) 3) (score-last-frame frame)
-      (and (strike? frame) (strike? next-frame)) 30
-      (and (strike? frame) (not (strike? next-frame))) (+ 20 (first next-frame))
-      :else (+ 10 (sum-frame frame)))))
-
-(defn score-spare [line index]
-  (let [frame (nth line index [0 0])]
-    (cond
-      (= (count frame) 3) (score-last-frame frame)
-      (strike? frame) 20
-      :else (+ 10 (first frame)))))
-
-(defn score-frame [line [frame-index frame]]
+(defn score-strike [frame next-frame]
   (cond
     (= (count frame) 3) (score-last-frame frame)
-    (strike? frame) (score-strike line (inc frame-index))
-    (spare? frame) (score-spare line (inc frame-index))
-    :else (sum-frame frame)))
+    (and (strike? frame) (strike? next-frame)) 30
+    (and (strike? frame) (not (strike? next-frame))) (+ 20 (first next-frame))
+    :else (+ 10 (sum-frame frame))))
+
+(defn score-spare [frame]
+  (cond
+    (= (count frame) 3) (score-last-frame frame)
+    (strike? frame) 20
+    :else (+ 10 (first frame))))
+
+(defn score-frame [line [frame-index current]]
+  (let [next-frame (nth line (+ frame-index 1) [0 0])
+        nnext-frame (nth line (+ frame-index 2) [0 0])]
+    (cond
+      (= (count current) 3) (score-last-frame current)
+      (strike? current) (score-strike next-frame nnext-frame)
+      (spare? current) (score-spare next-frame)
+      :else (sum-frame current))))
 
 (defn score [line]
   (let [game (line->data line)]
--- Bowling/clojure/bowling/test/bowling/core_test.clj
+++ Bowling/clojure/bowling/test/bowling/core_test.clj
@@ -2,8 +2,27 @@
   (:require [clojure.test :refer :all]
             [bowling.core :refer :all]))
 
-(deftest all-strikes-test
-  (are [line total] (= (score line) total)
-                    "X X X X X X X X X X X X" 300
-                    "9- 9- 9- 9- 9- 9- 9- 9- 9- 9-" 90
-                    "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5" 150))
+(deftest bowling
+  (testing "original test cases"
+    (are [line total] (= (score line) total)
+      "X X X X X X X X X XXX" 300
+      "9- 9- 9- 9- 9- 9- 9- 9- 9- 9-" 90
+      "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5" 150))
+
+  (testing "only open scores"
+    (is (= 99 (score "91 91 93 93 92 92 91 21 91 91"))))
+
+  (testing "open and misses"
+    (is (= 93 (score "91 9- 9- 9- 9- 9- 92 9- 9- 9-"))))
+
+  (testing "mixture of strikes and opens"
+    (is (= 98 (score "9- X 8- 9- 9- 9- 9- 9- 9- 9-")))
+    (is (= 104 (score "9- X 8- 9- 9- 9- 9- 9- 9- X23")))
+    (is (= 28 (score "-- X 81 -- -- -- -- -- -- --")))
+    (is (= 27 (score "-- X 8- 1- -- -- -- -- -- --"))))
+
+  (testing "mixture of spares and opens"
+    (is (= 82 (score "9- 8/ -- 9- 9- 9- 9- 9- 9- 9-")))
+    (is (= 84 (score "9- 8/ -- 9- 9- 9- 9- 9- 9- 9/1")))
+    (is (= 12 (score "-- 8/ 1- -- -- -- -- -- -- --")))
+    (is (= 11 (score "-- 8/ -1 -- -- -- -- -- -- --")))))
