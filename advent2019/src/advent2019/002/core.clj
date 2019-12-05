1884e55ede1348876f72a865d58b3dedff6299cb
kyleg <griersonky@gmail.com>
2019-12-05 16:57:28 +0000

    First transformation

--- advent2019/src/advent2019/002/core.clj
+++ advent2019/src/advent2019/002/core.clj
@@ -2,13 +2,7 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(defn- instruction?
-  [code]
-  (or (some? code) (not-empty code)))
-
-(defn execute [code]
-  (if (instruction? code)
-    (if (= (first code) \1)
-      "2,0,0,0"
-      code)
-    ""))
+(defn computer [program]
+  (if (nil? program)
+    ""
+    program))
--- advent2019/test/advent2019/002/core_test.clj
+++ advent2019/test/advent2019/002/core_test.clj
@@ -3,27 +3,24 @@
             [advent2019.002.core :refer :all]))
 
 (future-facts "Examples"
-              (execute "1,0,0,0,99" => "2,0,0,0,99")
-              (execute "2,3,0,3,99" => "2,3,0,6,99")
-              (execute "2,4,4,5,99,0" => "2,4,4,5,99,9801")
-              (execute "1,1,1,4,99,5,6,0,99" => "30,1,1,4,2,5,6,0,99"))
+              (computer "1,0,0,0,99") => "2,0,0,0,99"
+              (computer "2,3,0,3,99") => "2,3,0,6,99"
+              (computer "2,4,4,5,99,0") => "2,4,4,5,99,9801"
+              (computer "1,1,1,4,99,5,6,0,99") => "30,1,1,4,2,5,6,0,99")
 
 (facts "Silly"
-       (fact "empty"
-             (execute "") => ""
-             (execute nil) => "")
-       (fact "not valid instruction"
-             (execute "0") => "0"
-             (execute "0,1") => "0,1"
-             (execute "0,1,2") => "0,1,2")
-       (fact "opcode out of range"
-             (execute "3,0,0,0") => "3,0,0,0"))
+       (fact "given empty string should return empty string"
+             (computer "") => "")
+       (fact "given nil should return empty string"
+             (computer nil) => "")
+       (fact "intcode not instruction size should return intcode"
+             (computer "0") => "0"
+             (computer "0,1") => "0,1"
+             (computer "0,1,2") => "0,1,2")
+       (fact "opcode not 1,2, or 99 should return intcode"
+             (computer "0,0,0,0") => "0,0,0,0"
+             (computer "3,0,0,0") => "3,0,0,0"))
 
 (facts "Simple"
        (fact "halt instruction returns intcode"
-             (execute "99,0,0,0") => "99,0,0,0")
-       (fact "basic add [1 0 0 0]"
-             (execute "1,0,0,0") => "2,0,0,0")
-       (fact "basic add [1 1 1 0]"
-             (execute "1,1,1,0") => "1,2,1,1"))
-
+             (computer "99,0,0,0") => "99,0,0,0"))
