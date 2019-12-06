94909c76081db3dce5eaa292d439069fb718e2eb
kyleg <griersonky@gmail.com>
2019-12-06 14:09:49 +0000

    Passing all examples given in document

--- advent2019/src/advent2019/002/core.clj
+++ advent2019/src/advent2019/002/core.clj
@@ -3,19 +3,19 @@
             [clojure.string :as str]))
 
 (defn program->intcode [program]
-  (map #(Integer/parseInt %) (str/split program #",")))
+  (vec (map #(Integer/parseInt %) (str/split program #","))))
 
 (defn intcode->program [intcode]
   (str/join #"," intcode))
 
-(defn instruction? [intcode]
-  (>= (count intcode) 4))
+(defn instruction? [intcode index]
+  (>= (count (drop index intcode)) 4))
 
-(defn get-instruction [intcode]
-  (take 4 intcode))
+(defn get-instruction [intcode index]
+  (take 4 (drop index intcode)))
 
 (defn get-opcode [index]
-  (get {1 + 2 *} index))
+  (get {1 + 2 * 99 nil} index))
 
 (defn get-number [intcode index]
   (get intcode index))
@@ -23,16 +23,19 @@
 (defn update-intcode [intcode index value]
   (assoc intcode index value))
 
-(defn execute [intcode]
-  (if (instruction? intcode)
-    (let [[oi n1 n2 si] (get-instruction intcode)
-          opcode (get-opcode oi)
-          num1 (get-number intcode n1)
-          num2 (get-number intcode n2)]
-      (if (some? opcode)
-        (update-intcode intcode si (opcode num1 num2))
-        intcode))
-    intcode))
+(defn execute
+  ([intcode] (execute intcode 0))
+  ([intcode index]
+   (if (instruction? intcode index)
+     (let [[oi n1 n2 si] (get-instruction intcode index)
+           opcode (get-opcode oi)
+           num1 (get-number intcode n1)
+           num2 (get-number intcode n2)]
+       (if (some? opcode)
+         (execute (update-intcode intcode si (opcode num1 num2))
+                  (+ index 4))
+         intcode))
+     intcode)))
 
 (defn computer [program]
   (if (nil? program)
--- advent2019/test/advent2019/002/core_test.clj
+++ advent2019/test/advent2019/002/core_test.clj
@@ -2,11 +2,11 @@
   (:require [midje.sweet :refer :all]
             [advent2019.002.core :refer :all]))
 
-(future-facts "Examples"
-              (computer "1,0,0,0,99") => "2,0,0,0,99"
-              (computer "2,3,0,3,99") => "2,3,0,6,99"
-              (computer "2,4,4,5,99,0") => "2,4,4,5,99,9801"
-              (computer "1,1,1,4,99,5,6,0,99") => "30,1,1,4,2,5,6,0,99")
+(facts "Examples"
+       (computer "1,0,0,0,99") => "2,0,0,0,99"
+       (computer "2,3,0,3,99") => "2,3,0,6,99"
+       (computer "2,4,4,5,99,0") => "2,4,4,5,99,9801"
+       (computer "1,1,1,4,99,5,6,0,99") => "30,1,1,4,2,5,6,0,99")
 
 (fact "1,0,0,0,9 => 2,0,0,0,99"
       (computer "1,0,0,0,99") => "2,0,0,0,99"
@@ -23,26 +23,17 @@
       (intcode->program [1 0 0 0 99]) => "1,0,0,0,99"
       (intcode->program [1 1 1 1 99]) => "1,1,1,1,99")
 
-(fact "provided execute"
-      (execute [1 0 0 0 99]) => [2 0 0 0 99]
-      (provided
-       (instruction? [1 0 0 0 99]) => true
-       (get-instruction [1 0 0 0 99]) => [1 0 0 0]
-       (get-opcode 1) => +
-       (get-number [1 0 0 0 99] 0) => 1
-       (update-intcode [1 0 0 0 99] 0 2) => [2 0 0 0 99]))
-
 (facts "instruction"
-  (fact "valid"
-        (instruction? [1 0 0 0]) => true
-        (instruction? [1 0 0 0 99]) => true)
+       (fact "valid"
+             (instruction? [1 0 0 0] 0) => true
+             (instruction? [1 0 0 0 99] 0) => true)
 
-  (fact "invalid"
-        (instruction? [1 0 0]) => false))
+       (fact "invalid"
+             (instruction? [1 0 0] 0) => false))
 
 (facts "get-instruction"
        (fact "[1 0 0 0 99] => [1 0 0 0]"
-             (get-instruction [1 0 0 0 99]) => [1 0 0 0]))
+             (get-instruction [1 0 0 0 99] 0) => [1 0 0 0]))
 
 (facts "get-opcode"
        (fact "(get-opcode 1) => +"
