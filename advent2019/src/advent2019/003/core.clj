9cc19145cf4f20306488a4e57fc6209c4b026c64
Kyle Grierson <grierson@openmailbox.org>
2019-12-11 15:52:02 +0000

    003: Parse moves

--- advent2019/src/advent2019/003/core.clj
+++ advent2019/src/advent2019/003/core.clj
@@ -1,8 +1,28 @@
-(ns advent2019.003.core)
+(ns advent2019.003.core
+  (:require [clojure.string :as str]))
 
+
+(defn char->direction [c]
+  (case c
+    \R :right
+    \L :left
+    \U :up
+    \D :down))
+
+(defn str->move [[direction & amount]]
+  (let [distance (apply str amount)]
+    [(char->direction direction) (Integer/parseInt distance)]))
+
+(defn parse [wire]
+  (let [moves (str/split wire #",")]
+    (map str->move moves)))
+
+(defn mapping [parsed])
 
 (defn draw [wire]
-  [[1 0] [1 1]])
+  (-> wire
+      parse
+      mapping))
 
 (defn overlaps [wire1 wire2])
 (defn nearest [overlaps])
--- advent2019/test/advent2019/003/core_test.clj
+++ advent2019/test/advent2019/003/core_test.clj
@@ -2,7 +2,6 @@
   (:require [midje.sweet :refer :all]
             [advent2019.003.core :refer :all]))
 
-
 (fact "simple case"
       (intersection ..wire1.. ..wire2..) => [1 1]
       (provided
@@ -11,5 +10,20 @@
        (overlaps [[1 0] [1 1]] [[0 1] [1 1]]) => [[1 1]]
        (nearest [[1 1]]) => [1 1]))
 
-(fact "draw"
-      (draw "R1,U1") => [[1 0] [1 1]])
+(future-facts "draw wires"
+       (fact "R1,U1"
+             (draw "R1,U1") => [[1 0] [1 1]])
+       (fact "U1,R1"
+             (draw "U1,R1") => [[0 1] [1 1]]))
+
+(fact "R1,U1 provided"
+      (draw "R1,U1") => [[1 0] [1 1]]
+      (provided
+       (parse "R1,U1") => ..parsed..
+       (mapping ..parsed..) => [[1 0] [1 1]]))
+
+(fact "Parse"
+      (fact "R1"
+            (parse "R1") => [[:right 1]]
+            (provided
+             (str->move "R1") => [:right 1])))
