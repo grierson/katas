5e1bf797a7d8c7d2f4645c9aec81b9fb2f0f6134
Kyle Grierson <grierson@openmailbox.org>
2019-12-11 22:19:58 +0000

    003: Done

--- advent2019/src/advent2019/003/core.clj
+++ advent2019/src/advent2019/003/core.clj
@@ -1,5 +1,6 @@
 (ns advent2019.003.core
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
 
 (defn char->direction [c]
   (case c
@@ -20,8 +21,8 @@
   (case direction
     :right [(inc x) y]
     :left [(dec x) y]
-    :up [x (dec y )]
-    :down [x (inc y)])) 
+    :up [x (dec y)]
+    :down [x (inc y)]))
 
 (defn apply-move [location [direction amount]]
   (->> (iterate #(update-location direction %) location)
@@ -42,12 +43,32 @@
       parse
       mapping))
 
-(defn overlaps [wire1 wire2])
-(defn nearest [overlaps])
+(defn overlaps [wire1 wire2]
+  (clojure.set/intersection (set wire1) (set wire2)))
 
-(defn intersection [wire1 wire2]
+(defn distance [[x y]]
+  (+ (Math/abs x)
+     (Math/abs y)))
+
+(defn nearest [overlaps]
+  (->> overlaps
+       (map distance)
+       sort
+       second))
+
+(defn solve [wire1 wire2]
   (let [dw1 (draw wire1)
         dw2 (draw wire2)
         crossovers (overlaps dw1 dw2)
         closest (nearest crossovers)]
     closest))
+
+
+(defn problem1 []
+  (let [[wire1 wire2] (-> "003.txt"
+                          io/resource
+                          slurp
+                          str/split-lines)]
+    (solve wire1 wire2)))
+
+(problem1) 
--- advent2019/test/advent2019/003/core_test.clj
+++ advent2019/test/advent2019/003/core_test.clj
@@ -3,12 +3,12 @@
             [advent2019.003.core :refer :all]))
 
 (fact "intersection at bottom right [1 1] provided"
-      (intersection ..wire1.. ..wire2..) => [1 1]
+      (solve ..wire1.. ..wire2..) => [1 1]
       (provided
        (draw ..wire1..) => [[1 0] [1 1]]
        (draw ..wire2..) => [[0 1] [1 1]]
-       (overlaps [[1 0] [1 1]] [[0 1] [1 1]]) => [[1 1]]
-       (nearest [[1 1]]) => [1 1]))
+       (overlaps [[1 0] [1 1]] [[0 1] [1 1]]) => #{[1 1]}
+       (nearest #{[1 1]}) => [1 1]))
 
 (fact "draw provided"
       (draw "R1,D1") => [[1 0] [1 1]]
@@ -50,3 +50,22 @@
        (facts "two moves"
               (fact "right 1 up 1"
                     (mapping [[:right 1] [:up 1]]) => [[0 0] [1 0] [1 -1]])))
+
+(facts "overlaps"
+       (fact "one overlap"
+             (overlaps [[0 0] [1 0] [1 1]] [[0 0] [0 1] [1 1]]) => #{[0 0] [1 1]}))
+
+(facts "distance"
+       (fact "[1 1]"
+             (distance [1 1]) => 2))
+
+(fact "simple case"
+      (solve "R1,U1" "U1,R1") => 2)
+
+(fact "first example"
+      (solve "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+             "U62,R66,U55,R34,D71,R55,D58,R83") => 159)
+
+(fact "second example"
+      (solve "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+             "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7") => 135)
