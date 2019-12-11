ced86037e70c22e60401ad0f728a12d69ee6e4ff
Kyle Grierson <grierson@openmailbox.org>
2019-12-11 12:06:03 +0000

    003: Top Down Test

--- advent2019/src/advent2019/003/core.clj
+++ advent2019/src/advent2019/003/core.clj
@@ -1,2 +1,13 @@
 (ns advent2019.003.core)
 
+
+(defn draw [wire])
+(defn overlaps [wire1 wire2])
+(defn nearest [overlaps])
+
+(defn intersection [wire1 wire2]
+  (let [dw1 (draw wire1)
+        dw2 (draw wire2)
+        crossovers (overlaps dw1 dw2)
+        closest (nearest crossovers)]
+    closest))
--- advent2019/test/advent2019/003/core_test.clj
+++ advent2019/test/advent2019/003/core_test.clj
@@ -2,4 +2,11 @@
   (:require [midje.sweet :refer :all]
             [advent2019.003.core :refer :all]))
 
-(fact (+ 1 1) => 2)
+
+(fact "simple case"
+      (intersection ..wire1.. ..wire2..) => [1 1]
+      (provided
+       (draw ..wire1..) => [[1 0] [1 1]]
+       (draw ..wire2..) => [[0 1] [1 1]]
+       (overlaps [[1 0] [1 1]] [[0 1] [1 1]]) => [[1 1]]
+       (nearest [[1 1]]) => [1 1]))
