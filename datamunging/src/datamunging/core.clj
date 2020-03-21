497a9798e694de6ce81d08156e1ef024d9440018
Kyle Grierson <griersonky@gmail.com>
2020-03-21 14:58:22 +0000

    Done: football

--- datamunging/src/datamunging/core.clj
+++ datamunging/src/datamunging/core.clj
@@ -2,32 +2,58 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(def data (slurp (io/resource "weather.dat")))
+(def weather (slurp (io/resource "weather.dat")))
+(def football (slurp (io/resource "football.dat")))
+
+;; Helpers
 
 (defn strip-star [s]
   (apply str (filter (fn [c] (Character/isDigit c)) s)))
 
-(defn parse-record [record]
-  (->> record
-       (map strip-star)
-       (map #(Integer/parseInt %))))
+(defn str->int [s]
+  (Integer/parseInt (strip-star s)))
 
-(defn difference [{:keys [max min]}]
-  (- max min))
-
-(def body (->> (str/split-lines data)
-               (drop 2)
-               (drop-last 1)
-               (map #(str/split % #" "))
-               (map #(remove empty? %))
-               (map #(take 3 %))
-               (map parse-record)
-               (map #(zipmap [:day :max :min] %))))
+(defn keys->int [m [k & ks]]
+  (if (some? k)
+    (recur (assoc m k (str->int (get m k))) ks)
+    m))
 
 (defn find-biggest [records]
-  (:day (->> records
-             (map #(assoc % :diff (difference %)))
-             (apply max-key :diff))))
+  (:id (->> records
+            (map #(assoc % :diff (- (:max %) (:min %))))
+            (apply max-key :diff))))
 
-(find-biggest body)
+(defn find-gap [records]
+  (:id (->> records
+            (map #(assoc % :diff (- (:for %) (:against %))))
+            (apply max-key :diff))))
 
+(def weather-records (->> (str/split-lines weather)
+                          (drop 2)
+                          (drop-last 1)
+                          (map #(str/split % #" "))
+                          (map #(remove empty? %))
+                          (map #(take 3 %))
+                          (map #(zipmap [:id :max :min] %))
+                          (map #(keys->int % [:id :max :min]))))
+
+
+(defn ->football-record [data]
+  (let [team (nth data 1)
+        for (nth data 6)
+        against (nth data 8)]
+    {:id      team
+     :for     for
+     :against against}))
+
+(def football-records (->> (str/split-lines football)
+                           (drop 1)
+                           (map #(str/split % #" "))
+                           (map #(remove empty? %))
+                           (remove #(= 1 (count %)))
+                           (map ->football-record)
+                           (map #(keys->int % [:for :against]))))
+
+(comment
+  (find-biggest weather-records)
+  (find-gap football-records))
--- datamunging/test/datamunging/core_test.clj
+++ datamunging/test/datamunging/core_test.clj
@@ -2,17 +2,5 @@
   (:require [midje.sweet :refer :all]
             [datamunging.core :refer :all]))
 
-(fact "parse-record to ints"
-  (parse-record '("9", "86", "32")) => '(9, 86, 32))
-
-(future-fact "parse-record to ints"
-             (parse-record '("9", "86", "32*")) => '(9, 86, 32))
-
 (fact "strip stars"
   (strip-star "32*") => "32")
-
-(fact "mo end"
-  (strip-star "32*") => "32")
-
-(fact "difference"
-  (difference {:min 1 :max 2}) => 1)
