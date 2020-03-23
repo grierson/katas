891248482aa56e308e6a3c413cc293eaa77968a9
Kyle Grierson <griersonky@gmail.com>
2020-03-23 22:21:49 +0000

    Add rotation

--- rover/src/rover/core.clj
+++ rover/src/rover/core.clj
@@ -1,8 +1,28 @@
 (ns rover.core)
 
-(defn rotate-right [current]
-  (get {:N :E
-        :E :W} current))
+(defn rotate-right [state]
+  (update state :direction {:N :E
+                            :E :S
+                            :S :W
+                            :W :N}))
 
-(defn move [{:keys [direction]} moves]
-  {:direction (rotate-right direction)})
+(defn rotate-left [state]
+  (update state :direction {:N :W
+                            :E :N
+                            :S :E
+                            :W :S}))
+
+(defn update-state [state action]
+  (case action
+    \R (rotate-right state)
+    \L (rotate-left state)
+    state))
+
+(def start-point {:direction :N
+                  :x             0
+                  :y             0})
+
+(defn move [state [a & as]]
+  (if (nil? a)
+    state
+    (recur (update-state state a) as)))
