2ad3a2d79ed3935f992c8e89172fe096a089c25b
Kyle Grierson <griersonky@gmail.com>
2020-12-01 18:09:06 +0000

    2015 - 1 - part 2

--- advent/src/aoc2015/01.clj
+++ advent/src/aoc2015/01.clj
@@ -5,5 +5,15 @@
 
 (def count-if (comp count filter))
 
-(- (count-if #{\(} data)
-   (count-if #{\)} data))
+(comment
+  ;; first
+  (- (count-if #{\(} data)
+     (count-if #{\)} data)))
+
+(map-indexed vector data)
+
+(loop [cnt 0
+       [[idx v] & xs] (map-indexed vector data)]
+  (if (= cnt -1)
+    idx
+    (recur (if (= \( v) (inc cnt) (dec cnt)) xs)))
