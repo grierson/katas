bfe18520cf7070a4e96781e773f27a2d45c2435d
Kyle Grierson <griersonky@gmail.com>
2020-12-01 19:27:34 +0000

    2015 - 2 - part 1

--- advent/src/aoc/common.clj
+++ advent/src/aoc/common.clj
@@ -1,5 +1,8 @@
 (ns aoc.common
   (:require [clojure.java.io :as io]))
 
-(defn read [file]
+(defn read-file [file]
   (line-seq (io/reader (io/resource file))))
+
+
+(defn parse-int [s] (Long/parseLong s))
---
+++ advent/src/aoc2015/02.clj
@@ -0,0 +1,19 @@
+(ns aoc2015.02
+  (:require [aoc.common :refer [read-file parse-int]]
+            [clojure.string :as str]))
+
+(def data (read-file "aoc2015/02.txt"))
+
+(defn paper [[l w h]]
+  (let [side1 (* l w)
+        side2 (* w h)
+        side3 (* h l)]
+    (+ (min side1 side2 side3)
+       (* 2 side1)
+       (* 2 side2)
+       (* 2 side3))))
+
+(defn parse-measurement [measurement]
+  (map parse-int (str/split measurement #"x")))
+
+(reduce + (map (comp paper parse-measurement) data))
