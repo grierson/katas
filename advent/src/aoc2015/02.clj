4f48b651323867dee3ec2d15b0f50051df32c84a
Kyle Grierson <griersonky@gmail.com>
2020-12-01 19:54:11 +0000

    refactor: 2015 - 2 - part 2

--- advent/src/aoc2015/02.clj
+++ advent/src/aoc2015/02.clj
@@ -13,10 +13,11 @@
        (* 2 side2)
        (* 2 side3))))
 
-(defn ribbon [[l w h :as dimensions]]
-  (let [[s1 s2] (take 2 (sort dimensions))]
-    (+ (+ s1 s1 s2 s2)
-       (* l w h))))
+(defn ribbon [dimensions]
+  (let [[x y z] (sort dimensions)]
+    (+ (* 2 x)
+       (* 2 y)
+       (* x y z))))
 
 (defn parse-measurement [measurement]
   (map parse-int (str/split measurement #"x")))
