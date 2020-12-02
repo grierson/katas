71c410c8466ad95fd9be0001ab2cbfc4865fa97b
Kyle Grierson <griersonky@gmail.com>
2020-12-02 13:28:50 +0000

    refactor: 2020 - 2 - part 2

--- advent/src/aoc2020/02.clj
+++ advent/src/aoc2020/02.clj
@@ -1,35 +1,23 @@
 (ns aoc2020.02
   (:require [aoc.common :refer [read-file
                                 count-if
-                                parse-int]]
-            [clojure.string :as str]))
+                                parse-int]]))
 
 (def data (read-file "aoc2020/02.txt"))
 
 (defn parse-line [line]
-  (let [parts (str/split line #" ")
-        limits (map parse-int (str/split (first parts) #"-"))
-        c (first (second parts))
-        password (last parts)]
-    [limits c password]))
+  (let [[_ low high c password] (re-find #"(\d+)-(\d+) (.): (.*)" line)]
+    [(parse-int low) (parse-int high) (first c) password]))
 
-(defn valid-password? [[[low high] c password]]
+(defn valid-password? [[low high c password]]
   (let [cnt (count-if #{c} password)]
-    (and (>= cnt low)
-         (<= cnt high))))
+    (<= low cnt high)))
 
 (count-if true? (map (comp valid-password? parse-line) data))
 
-(defn valid-password2? [[[low high] c password]]
-  (let [temp (cons \_ password)
-        x (= c (nth temp low))
-        y (= c (nth temp high))]
+(defn valid-password2? [[low high c password]]
+  (let [x (= c (nth password (dec low)))
+        y (= c (nth password (dec high)))]
     (not= x y)))
 
 (count-if true? (map (comp valid-password2? parse-line) data))
-
-(comment
-  ((comp valid-password2? parse-line) "1-3 a: abcde")       ;; valid
-  ((comp valid-password2? parse-line) "1-3 b: cdefg")       ;; invalid
-  ((comp valid-password2? parse-line) "2-9 c: ccccccccc"))  ;; invalid
-
