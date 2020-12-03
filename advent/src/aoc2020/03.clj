f9db78a05c23f43eae4b4547a5c45e4d4481da53
Kyle Grierson <griersonky@gmail.com>
2020-12-03 21:08:57 +0000

    done: 2020 - 3 - part 2

--- advent/src/aoc2020/03.clj
+++ advent/src/aoc2020/03.clj
@@ -4,5 +4,12 @@
 
 (def data (read-file "aoc2020/03.txt"))
 
-(count-if #{\#} (map (fn [line n] (nth (cycle line) n)) data (iterate #(+ 3 %) 0)))
+(defn location [x y] (nth (cycle x) y))
+(def count-trees #(count-if #{\#} %))
 
+(let [a (count-trees (map location data (iterate #(+ 1 %) 0)))
+      b (count-trees (map location data (iterate #(+ 3 %) 0)))
+      c (count-trees (map location data (iterate #(+ 5 %) 0)))
+      d (count-trees (map location data (iterate #(+ 7 %) 0)))
+      e (count-trees (map location (take-nth 2 data) (iterate #(+ 1 %) 0)))]
+  (* a b c d e))
