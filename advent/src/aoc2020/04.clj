eb877fad24b07e20f3c5532583e3b57b201bcd40
Kyle Grierson <griersonky@gmail.com>
2020-12-05 00:38:33 +0000

    refactor: 2020 - 4 - part 2

--- advent/src/aoc2020/04.clj
+++ advent/src/aoc2020/04.clj
@@ -1,68 +1,49 @@
 (ns aoc2020.04
-  (:require [advent.core :refer [count-if]]
+  (:require [advent.core :refer [count-if parse-int]]
             [clojure.set :refer [superset?]]
             [clojure.string :as str]
             [clojure.java.io :as io]))
 
 (def input (slurp (io/resource "aoc2020/04.txt")))
+(def data input)
 
 (def req #{"byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"})
 
-(defn input->lines [lines]
-  (->> (str/split lines #"\R\R")
-       (map #(str/replace % "\n" " "))
-       (map #(str/split % #" "))))
+(defn line->passport [line]
+  (into {} (map (comp vec next)) (re-seq #"(\w{3}):(\S+)" line)))
 
-(defn get-keys [line]
-  (set (map #(subs % 0 3) line)))
+(defn valid-passport? [passport]
+  (superset? (set (keys passport)) req))
 
-(defn solve1 [data]
-  (count-if #(superset? % req) (map get-keys (input->lines data))))
-
-(defn valid-byr? [byr] (if byr (<= 1920 (Integer/parseInt byr) 2002) false))
-(defn valid-iyr? [iyr] (if iyr (<= 2010 (Integer/parseInt iyr) 2020) false))
-(defn valid-eyr? [eyr] (if eyr (<= 2020 (Integer/parseInt eyr) 2030) false))
-(defn valid-hcl? [hcl] (when hcl (some? (re-find #"^(#)([a-f0-9]{6})" hcl))))
-(defn valid-ecl? [ecl] (when ecl (some? (re-find #"(amb|blu|brn|gry|grn|hzl|oth)" ecl))))
-(defn valid-pid? [pid] (when pid (some? (re-find #"([0-9]{9})" pid))))
-
-(defn parse-hgt [hgt]
-  (if hgt
-    (let [[_ amount unit :as result] (re-find #"(\d+)(cm|in)" hgt)]
-      (when result
-        [(Integer/parseInt amount) unit]))))
+(defn solve [data]
+  (->> (str/split data #"\R\R")
+       (map line->passport)
+       (count-if valid-passport?)))
 
 (defn valid-hgt? [hgt]
-  (if-let [[amount unit] (parse-hgt hgt)]
-    (condp = unit
-      "cm" (<= 150 amount 193)
-      "in" (<= 59 amount 76))
-    false))
+  (let [[_ amount unit] (re-find #"(\d+)(cm|in)" hgt)]
+    (case unit
+      "cm" (<= 150 (parse-int amount) 193)
+      "in" (<= 59 (parse-int amount) 76)
+      false)))
 
-(defn parse-field [field]
-  (when-let [[_ k v] (re-find #"([a-z]+):(\S+)" field)]
-    [k v]))
+(defn valid-passport2? [{:strs [byr iyr eyr hgt hcl ecl pid] :as passport}]
+  (and
+    (valid-passport? passport)
+    (<= 1920 (parse-int byr) 2002)
+    (<= 2010 (parse-int iyr) 2020)
+    (<= 2020 (parse-int eyr) 2030)
+    (re-find #"^(#)([a-f0-9]{6})" hcl)
+    (re-find #"^(amb|blu|brn|gry|grn|hzl|oth)" ecl)
+    (re-find #"^\d{9}$" pid)
+    (valid-hgt? hgt)))
 
-(defn line->passport [passport]
-  (into (sorted-map) (map parse-field passport)))
-
-(defn valid-passport? [{:strs [byr iyr eyr hgt hcl ecl pid]}]
-  (and (valid-byr? byr)
-       (valid-iyr? iyr)
-       (valid-eyr? eyr)
-       (valid-hgt? hgt)
-       (valid-hcl? hcl)
-       (valid-ecl? ecl)
-       (valid-pid? pid)))
-
-(count-if valid-passport? (map line->passport (input->lines input)))
-(map line->passport (input->lines input))
+(defn solve2 [data]
+  (->> (str/split data #"\R\R")
+       (map line->passport)
+       (count-if valid-passport2?)))
 
 (comment
-  (valid-passport? {"eyr" "1972" "cid" "100" "hcl" "#18171d" "ecl" "amb" "hgt" "170" "pid" "186cm" "byr" "1926"})
-  (valid-passport? {"iyr" "2019" "hcl" "#602927" "eyr" "1967" "hgt" "170cm" "ecl" "grn" "pid" "012533040" "byr" "1946"})
-  (valid-passport? {"hcl" "dab227" "iyr" "2012" "ecl" "brn" "hgt" "182cm" "pid" "021572410" "eyr" "2020" "byr" "1992" "cid" "277"})
-  (valid-passport? {"hgt" "59cm" "ecl" "zzz" "eyr" "2038" "hcl" "74454a" "iyr" "2023" "pid" "3556412378" "byr" "2007"}))
+  (solve input)
+  (solve2 input))
 
-(comment
-  (solve1 input))
