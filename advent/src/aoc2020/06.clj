032fbe25e96dd5068024d0444c5c7bfd8aacc82b
Kyle Grierson <griersonky@gmail.com>
2020-12-06 10:24:16 +0000

    refactor: 2020 - 6 - part 2

--- advent/src/aoc2020/06.clj
+++ advent/src/aoc2020/06.clj
@@ -1,16 +1,28 @@
 (ns aoc2020.06
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
 (def input  (slurp (io/resource "aoc2020/06.txt")))
 
+(defn solve [line]
+  ((comp count set #(str/replace % "\n" "")) line))
+
 (comment
-  (reduce + (map (comp count set #(str/replace % "\n" "")) (str/split input #"\R\R"))))
+  (reduce + (map solve (str/split input #"\R\R"))))
 
 (defn solve2 [line]
+  "My bad implementation of intersection before I knew about it :)"
   (let  [y (frequencies line)
          groups (inc (get y \newline 0))]
     (count (filter (fn [[_ cnt]] (= groups cnt)) y))))
 
+(defn solve22 [line]
+  (->> line
+       str/split-lines
+       (map set)
+       (apply set/intersection)
+       count))
 
-(reduce + (map solve2 (str/split input #"\R\R")))
+(comment
+  (reduce + (map solve22 (str/split input #"\R\R"))))
