2fc568278518bfb5ebb54acedcf512717c19a65d
Kyle Grierson <griersonky@gmail.com>
2020-12-06 09:45:40 +0000

    done: 2020 - 6 - part 2

--- advent/src/aoc2020/06.clj
+++ advent/src/aoc2020/06.clj
@@ -4,5 +4,13 @@
 
 (def input  (slurp (io/resource "aoc2020/06.txt")))
 
-(reduce + (map (comp count set #(str/replace % "\n" "")) (str/split input #"\R\R")))
+(comment
+  (reduce + (map (comp count set #(str/replace % "\n" "")) (str/split input #"\R\R"))))
 
+(defn solve2 [line]
+  (let  [y (frequencies line)
+         groups (inc (get y \newline 0))]
+    (count (filter (fn [[_ cnt]] (= groups cnt)) y))))
+
+
+(reduce + (map solve2 (str/split input #"\R\R")))
