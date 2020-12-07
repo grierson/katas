78468a80f0c3fc70bc621957ac250a647f8f2d36
Kyle Grierson <griersonky@gmail.com>
2020-12-07 21:55:11 +0000

    init: 2020 - 7 - part 1

---
+++ advent/src/aoc2020/07.clj
@@ -0,0 +1,19 @@
+(ns aoc2020.07
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def input (slurp (io/resource "aoc2020/07sample.txt")))
+
+(def temp (-> input
+              str/split-lines))
+
+(defn get-colors [line]
+  (let [result (re-seq #"(\w+) (\w+) (bag|bags)" line)
+        colors (map (fn [[_ c1 c2 _]] (str/join #" " [c1 c2])) result)
+        deps (rest colors)]
+    (if (= deps '("no other"))
+      [(first colors) nil]
+      [(first colors) deps])))
+
+(map get-colors temp)
+
