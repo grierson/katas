5e78ff44c71cedf6ed46af4a7ee0cca49f44a19c
Kyle Grierson <griersonky@gmail.com>
2020-12-13 12:27:38 +0000

    init: 2020 - 10 - part 1

--- advent/src/aoc2020/11.clj
+++ advent/src/aoc2020/11.clj
@@ -15,27 +15,73 @@
 ; [0 0] [_ _] [2 0]
 ; [0 1]  [1 1] [2 1]
 
-(defn get-neighbours [[x y] max-x max-y]
+(defn neighbour-locations
+  "int int [int int] -> [[int int]]
+   ===
+   Returns all valid surrounding spaces"
+  [max-x max-y [x y]]
   (for [*x (range (max 0 (dec x)) (inc (min max-x (inc x))))
         *y (range (max 0 (dec y)) (inc (min max-y (inc y))))
         :when (not (and (= x *x) (= y *y)))]
-    [*x *y]))
+    [*y *x]))
 
-(defn neighbour->value [lines neighbour]
+(defn neighbour->value
+  "[string] [int int] -> char
+   ===
+   Returns value at position within lines"
+  [lines neighbour]
   (get-in lines neighbour))
 
-(defn neighbours->values [lines neighbours]
+(defn neighbours->values
+  "[string] [[int int]] -> [char]
+   ===
+   Get all values of surrounding space"
+  [lines neighbours]
   (map #(neighbour->value lines %) neighbours))
 
-(defn crowded? [neighbours]
+(defn crowded?
+  "4 or more occupied (#) seats"
+  [neighbours]
   (let [occupied (count (filter #{\#} neighbours))]
     (>= occupied 4)))
 
-(defn open? [neighbours]
+(defn open?
+  "No occupied (#) seats"
+  [neighbours]
   (not-any? #{\#} neighbours))
 
-(crowded? (neighbours->values lines (get-neighbours [0 0] 10 10)))
-(open? (neighbours->values lines (get-neighbours [0 0] 10 10)))
+(defn get-neighbours
+  "[string] int int [int int] -> [char]
+   ===
+   Given lines, boundaries (max-x max-y), position ([x y])
+   Then return all values surrounding position"
+  [lines max-x max-y position]
+  (neighbours->values lines (neighbour-locations max-x max-y position)))
+
+(crowded? (get-neighbours lines 10 10 [0 0]))
+(open? (get-neighbours lines 10 10 [0 0]))
+
+(defn apply-rules
+  "[char] char -> char
+   ===
+   Return value for position given neighbours values"
+  [neighbours value]
+  (condp = value
+    \# (if (crowded? neighbours) \L value)
+    \L (if (open? neighbours) \# value)
+    :else value))
+
+lines
+
+(get-in lines [6 1])
+
+(let [location [0 1]]
+  (apply-rules (get-neighbours lines 10 10 location) (get-in lines location)))
+
+; L -> open? true => #
+; # -> crowded? true => L
+; x -> x
+
 
 (comment
   (let [max-x (dec (count (first lines)))
