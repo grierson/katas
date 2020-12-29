6ccd802b2b9c0fe3cfa6be0a1ffd526475594a4c
Kyle Grierson <griersonky@gmail.com>
2020-12-29 15:28:41 +0000

    done: roman numerals with recursion

--- romannumerals/src/romannumerals/core.clj
+++ romannumerals/src/romannumerals/core.clj
@@ -3,7 +3,6 @@
 (defn number->roman [number]
   (cond
     (= number 0) ""
-    (<= number 3) (apply str (take number (repeat "I")))
     (>= number 1000) (str "M" (number->roman (- number 1000)))
     (>= number 500) (str "D" (number->roman (- number 500)))
     (>= number 100) (str "C" (number->roman (- number 100)))
@@ -13,7 +12,8 @@
     (>= number 10) (str "X" (number->roman (- number 10)))
     (>= number 9) (str "IX" (number->roman (- number 9)))
     (>= number 5) (str "V" (number->roman (- number 5)))
-    (>= number 4) (str "IV" (number->roman (- number 4)))))
+    (>= number 4) (str "IV" (number->roman (- number 4)))
+    (>= number 1) (str "I" (number->roman (- number 1)))))
 
 (def roman (sorted-map-by >
                           1000 "M"
@@ -33,9 +33,7 @@
   ([state [[v sym] & tail] number]
    (if (zero? number)
      state
-     (recur (let [result (quot number v)]
-              (if (zero? result)
-                state
-                (apply str state (repeat (quot number v) sym))))
-            tail
-            (rem number v)))))
+     (let [[how-many-times remainder] ((juxt quot rem) number v)]
+       (recur (apply str state (repeat how-many-times sym))
+              tail
+              remainder)))))
