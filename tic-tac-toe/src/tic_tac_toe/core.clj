03d237c5617d156e025251530125be8b3768e8e6
Kyle Grierson <griersonky@gmail.com>
2021-01-01 23:45:25 +0000

    tic-tac-toe
    refactor

--- tic-tac-toe/src/tic_tac_toe/core.clj
+++ tic-tac-toe/src/tic_tac_toe/core.clj
@@ -1,48 +1,23 @@
 (ns tic-tac-toe.core)
 
-(defn check-row [row]
+(defn three-in-a-row? [row]
   (when (and (apply = row)
              (not (.contains row \_)))
     (first row)))
 
-(defn check-column [column]
-  (when (and (apply = column)
-             (not (.contains column \_)))
-    (first column)))
-
-(defn check-columns [game]
+(defn get-columns [game]
   (let [left-column (map first game)
         middle-column (map second game)
         right-column (map last game)]
-    (some identity (map check-column [left-column middle-column right-column]))))
+    [left-column middle-column right-column]))
 
-(defn check-forward-slash [[[tl]
-                            [_ m]
-                            [_ _ br]]]
-  (let [diagonal [tl m br]]
-    (when (and (apply = diagonal)
-               (not (.contains diagonal \_)))
-      tl)))
-
-(defn check-back-slash [[[_ _ tr]
-                         [_ m]
-                         [bl]]]
-  (let [diagonal [tr m bl]]
-    (when (and (apply = diagonal)
-               (not (.contains diagonal \_)))
-      tr)))
-
-(defn check-diagonals [game]
-  (or (check-forward-slash game)
-      (check-back-slash game)))
-
-(defn check [game]
-  (let [rows (some check-row game)
-        columns (check-columns game)
-        diagonals (check-diagonals game)]
-    (or rows columns diagonals)))
+(defn get-diagonals [[[tl _ tr]
+                      [_ m _]
+                      [bl _ br]]]
+  [[tl m br] [tr m bl]])
 
 (defn score [game]
-  (if (nil? game)
-    nil
-    (check game)))
+  (when (some? game)
+    (let [columns (get-columns game)
+          diagonals (get-diagonals game)]
+      (some three-in-a-row? (concat game columns diagonals)))))
