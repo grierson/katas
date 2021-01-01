3d61fe0ce41f8463845c5a95f207e2268ff9fac9
Kyle Grierson <griersonky@gmail.com>
2021-01-01 22:39:52 +0000

    tic-tac-toe
    refactor: rows check

--- tic-tac-toe/src/tic_tac_toe/core.clj
+++ tic-tac-toe/src/tic_tac_toe/core.clj
@@ -1,8 +1,9 @@
 (ns tic-tac-toe.core)
 
 (defn check [game]
-  (some (fn [[l m r]] (if (and (= l m r)
-                               (not= l \_)) l)) game))
+  (let [rows (some (fn [[l m r]] (if (and (= l m r)
+                                          (not= l \_)) l)) game)]
+    (or rows)))
 (defn score [game]
   (if (nil? game)
     nil
