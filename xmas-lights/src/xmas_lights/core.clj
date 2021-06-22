a717fcb06ffa61ecd763c84d03cb095b026d7314
Kyle Grierson <griersonky@gmail.com>
2021-06-22 14:40:01 +0100

    Turn on light

--- xmas-lights/src/xmas_lights/core.clj
+++ xmas-lights/src/xmas_lights/core.clj
@@ -1,9 +1,11 @@
 (ns xmas-lights.core)
 
 
-(defn make-grid
-  ([] [])
-  ([columns rows]
-   (for [_ (range 0 columns)]
-     (for [_ (range 0 rows)]
-       false))))
+(defn make-grid [columns rows]
+  (->> (repeat columns false)
+       (into [])
+       (repeat rows)
+       (into [])))
+
+(defn turn-on [grid coords]
+  (update-in grid coords not))
--- xmas-lights/test/xmas_lights/core_test.clj
+++ xmas-lights/test/xmas_lights/core_test.clj
@@ -3,10 +3,12 @@
             [xmas-lights.core :refer :all]))
 
 (fact "make grid"
-  (make-grid) => []
   (make-grid 1 1) => [[false]]
   (make-grid 2 2) => [[false false]
                       [false false]]
-  (make-grid 2 1) => [[false]
-                      [false]]
-  (make-grid 1 2) => [[false false]])
+  (make-grid 2 1) => [[false false]]
+  (make-grid 1 2) => [[false] [false]])
+
+(fact "turn on light"
+  (let [grid (make-grid 1 1)]
+    (turn-on grid [0 0]) => [[true]]))
