9622083b47db7bc85b231eb618a044344f9c7e72
Kyle Grierson <griersonky@gmail.com>
2021-07-14 11:15:49 +0100

    Add tests

--- LeetCode/Easy/1-Two-Sum/main.rkt
+++ LeetCode/Easy/1-Two-Sum/main.rkt
@@ -1,3 +1,6 @@
+#lang racket
+(require rackunit)
+
 (define (make-dict coll)
   (define (foo state index xs)
     (if (empty? xs)
@@ -6,10 +9,22 @@
   (apply hash (foo '() 0 coll)))
 
 (define (two-sum nums target)
-  (define (foo d index)
-    (let* ([complement (- target (list-ref nums index))]
-           [x (hash-ref d complement null)])
-      (if (and (not (null? x)) (not (= x index))) 
-          (list index x)
-          (foo d (add1 index)))))
-  (foo (make-dict nums) 0))
+  (let* ([vnums (list->vector nums)]
+         [lvnums (sub1 (vector-length vnums))])
+    (define (foo d index)
+      (let* ([complement (- target (vector-ref vnums index))]
+             [x (hash-ref d complement null)])
+        (cond
+          [(and (not (null? x)) (not (= x index))) (list index x)]
+          [(= index lvnums) null]
+          [else (foo d (add1 index))])))
+    (foo (make-dict nums) 0)))
+
+(check-equal?
+ (two-sum (list 2 7 11 15) 9) (list 0 1))
+
+(check-equal?
+ (two-sum (list 3 2 4) 6) (list 1 2))
+
+(check-equal?
+ (two-sum (list 3 3) 6) (list 0 1))
