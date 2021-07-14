db45a7bf703b1820412422c694ed57ee8d75ee6a
Kyle Grierson <griersonky@gmail.com>
2021-07-14 10:22:48 +0100

    LeetCode: 21 Merge two lists

--- LeetCode/Easy/21-Merge-Two-Sorted-Lists/main.rkt
+++ LeetCode/Easy/21-Merge-Two-Sorted-Lists/main.rkt
@@ -1,19 +1,25 @@
 #lang racket
 (require rackunit)
 
-(struct list-node 
+(struct list-node
   (val next) #:mutable #:transparent)
 
-(define (make-list-node [val 0])
-  (list-node val #f))
+(define (mkn val [next #f])
+  (list-node val next))
 
 ;; -----
 
-(define (merge-two-lists l1 l2)
-  (make-list-node))
+(define/match (merge-two-lists l1 l2)
+  [(#f #f) #f]
+  [(n #f) n]
+  [(#f n) n]
+  [((list-node l1v l1n) (list-node l2v l2n))
+   (if (< l1v l2v)
+       (list-node l1v (merge-two-lists l1n l2))
+       (list-node l2v (merge-two-lists l1 l2n)))])
 
 
-; Empty tests
-(check-equal? (merge-two-lists (make-list-node) (make-list-node)) (make-list-node))
-(check-equal? (merge-two-lists (make-list-node 1) (make-list-node)) (make-list-node 1))
-(check-equal? (merge-two-lists (make-list-node) (make-list-node 1)) (make-list-node 1))
+(check-equal?
+ (merge-two-lists (mkn 1 (mkn 2 (mkn 4)))
+                  (mkn 1 (mkn 3 (mkn 4))))
+ (mkn 1 (mkn 1 (mkn 2 (mkn 3 (mkn 4 (mkn 4)))))))
