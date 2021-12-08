608447905ce82a65c2f216ea19c2e55062905300
Kyle Grierson <kyle.grierson@mypulse.ai>
2021-12-08 09:22:15 +0000

    aoc2021: 04 total board 1

--- advent/src/aoc2021/04.clj
+++ advent/src/aoc2021/04.clj
@@ -3,44 +3,50 @@
 
 (def sample "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1\n\n22 13 17 11  0\n 8  2 23  4 24\n21  9 14 16  7\n 6 10  3 18  5\n 1 12 20 15 19\n\n 3 15  0  2 22\n 9 18 13 17  5\n19  8  7 25 23\n20 11 10 24  4\n14 21 16 12  6\n\n14 21 17 24  4\n10 16 15  9 19\n18  8 23 26 20\n22 11 13  6  5\n 2  0 12  3  7")
 
-(defn line->data [line]
-  (map (comp #(hash-map :value %
-                        :marked false) parse-long) (re-seq #"\d+" line)))
 
-(defn board->data [board]
-  (map line->data board))
+(defn data->board [data]
+  (map (comp #(hash-map :number %
+                        :marked? false)
+             parse-long) data))
+
+(defn data->boards [board]
+  (map data->board (map #(re-seq #"\d+" %) board)))
+
+(def data (str/split-lines sample))
 
 (defn input->data [input]
-  (let [data (str/split-lines input)]
-    {:numbers (map parse-long (str/split (first data) #","))
-     :boards  (map board->data (partition 5 (filter not-empty (drop 2 data))))}))
+  (let [data (str/split-lines input)
+        numbers (str/split (first data) #",")
+        boards (map #(str/join " " %) (partition 5 (filter not-empty (drop 2 data))))]
+    {:numbers (map parse-long numbers)
+     :boards  (data->boards boards)}))
 
-(defn mark-line [line called-number]
-  (map (fn [{:keys [value] :as number}]
-         (if (= value called-number)
-           (update number :marked not)
-           number))
-       line))
+(defn mark-number [{:keys [number] :as item} called-number]
+  (if (= number called-number)
+    (update item :marked? not)
+    item))
 
 (defn mark-board [board called-number]
-  (map #(mark-line % called-number) board))
+  (map #(mark-number % called-number) board))
 
-(defn all-marked? [numbers]
-  (every? :marked numbers))
+(defn all-marked? [items]
+  (every? :marked? items))
 
-(defn bingo-row? [board]
-  (some true? (map all-marked? board)))
+(defn get-rows [board]
+  (partition 5 board))
 
-(defn bingo-column? [board]
-  (let [column1 (map (fn [line] (nth line 0 nil)) board)
-        column2 (map (fn [line] (nth line 1 nil)) board)
-        column3 (map (fn [line] (nth line 2 nil)) board)
-        column4 (map (fn [line] (nth line 3 nil)) board)
-        column5 (map (fn [line] (nth line 4 nil)) board)
-        columns (vector column1 column2 column3 column4 column5)]
-    (some true? (map all-marked? columns))))
+(defn get-columns [board]
+  (let [columns (map #(drop % board) (range 5))]
+    (map #(take-nth 5 %) columns)))
 
 (defn bingo? [board]
-  (or
-    (bingo-row? board)
-    (bingo-column? board)))
+  (let [rows (get-rows board)
+        columns (get-columns board)
+        permutations (concat rows columns)]
+    (some all-marked? permutations)))
+
+(defn total [board]
+  (->> board
+       (remove :marked?)
+       (map :number)
+       (apply +)))
--- advent/test/aoc2021/04_test.clj
+++ advent/test/aoc2021/04_test.clj
@@ -3,84 +3,50 @@
             [aoc2021.04 :refer :all]))
 
 (def first-sample-board
-  [[{:marked false, :value 22} {:marked false, :value 13} {:marked false, :value 17} {:marked false, :value 11} {:marked false, :value 0}]
-   [{:marked false, :value 8} {:marked false, :value 2} {:marked false, :value 23} {:marked false, :value 4} {:marked false, :value 24}]
-   [{:marked false, :value 21} {:marked false, :value 9} {:marked false, :value 14} {:marked false, :value 16} {:marked false, :value 7}]
-   [{:marked false, :value 6} {:marked false, :value 10} {:marked false, :value 3} {:marked false, :value 18} {:marked false, :value 5}]
-   [{:marked false, :value 1} {:marked false, :value 12} {:marked false, :value 20} {:marked false, :value 15} {:marked false, :value 19}]])
-
-
-(defn get-board-line [board line]
-  (nth board line))
-
-(deftest line->data-test
-  (is (= (get-board-line first-sample-board 0)
-         (line->data "22 13 17 11  0"))))
+  [{:marked? false, :number 22} {:marked? false, :number 13} {:marked? false, :number 17} {:marked? false, :number 11} {:marked? false, :number 0}
+   {:marked? false, :number 8} {:marked? false, :number 2} {:marked? false, :number 23} {:marked? false, :number 4} {:marked? false, :number 24}
+   {:marked? false, :number 21} {:marked? false, :number 9} {:marked? false, :number 14} {:marked? false, :number 16} {:marked? false, :number 7}
+   {:marked? false, :number 6} {:marked? false, :number 10} {:marked? false, :number 3} {:marked? false, :number 18} {:marked? false, :number 5}
+   {:marked? false, :number 1} {:marked? false, :number 12} {:marked? false, :number 20} {:marked? false, :number 15} {:marked? false, :number 19}])
 
 (deftest board->data-test
   (is (= first-sample-board
-         (board->data '("22 13 17 11  0"
-                         " 8  2 23  4 24"
-                         "21  9 14 16  7"
-                         " 6 10  3 18  5"
-                         " 1 12 20 15 19")))))
+         (data->board '("22" "13" "17" "11" "0" "8" "2" "23" "4" "24" "21" "9" "14" "16" "7" "6" "10" "3" "18" "5" "1" "12" "20" "15" "19")))))
 
 (deftest input->data-test
   (is (= first-sample-board
          (first (get (input->data sample) :boards)))))
 
-(deftest mark-line-test
-  (is (= [{:marked false, :value 21}
-          {:marked false, :value 9}
-          {:marked false, :value 14}
-          {:marked false, :value 16}
-          {:marked true, :value 7}]
-         (mark-line (get-board-line first-sample-board 2) 7))))
-
 (deftest mark-board-test
-  (let [line1 (get-board-line first-sample-board 0)
-        line2 (get-board-line first-sample-board 1)
-        line3 [{:marked false, :value 21}
-               {:marked false, :value 9}
-               {:marked false, :value 14}
-               {:marked false, :value 16}
-               {:marked true, :value 7}]
-        line4 (get-board-line first-sample-board 3)
-        line5 (get-board-line first-sample-board 4)]
-    (is (= (vector line1 line2 line3 line4 line5)
+  (let [new-board (update-in first-sample-board [14 :marked?] not)]
+    (is (= new-board
            (mark-board first-sample-board 7)))))
 
 (deftest all-marked?-test
   (testing "all marked"
-    (is (true? (all-marked? [{:marked true, :value 21}
-                             {:marked true, :value 9}
-                             {:marked true, :value 14}
-                             {:marked true, :value 16}
-                             {:marked true, :value 7}]))))
-  (testing "none marked"
-    (is (false? (all-marked? [{:marked false, :value 21}
-                              {:marked false, :value 9}
-                              {:marked false, :value 14}
-                              {:marked false, :value 16}
-                              {:marked false, :value 7}])))))
+    (is (true? (all-marked? [{:marked? true, :number 22}
+                             {:marked? true, :number 8}]))))
+  (testing "none marked?"
+    (is (false? (all-marked? [{:marked? false, :number 21}
+                              {:marked? false, :number 9}]))))
+  (testing "some marked? is still false"
+    (is (false? (all-marked? [{:marked? false, :number 21}
+                              {:marked? true, :number 9}])))))
 
 (deftest bingo?-test
-  (testing "line complete"
-    (let [line1 [{:marked true, :value 22}
-                 {:marked true, :value 13}
-                 {:marked true, :value 17}
-                 {:marked true, :value 11}
-                 {:marked true, :value 0}]
-          line2 (get-board-line first-sample-board 1)
-          line3 (get-board-line first-sample-board 2)
-          line4 (get-board-line first-sample-board 3)
-          line5 (get-board-line first-sample-board 4)]
-      (is (true? (bingo? (vector line1 line2 line3 line4 line5))))))
+  (testing "row complete"
+    (let [board [{:marked? true, :number 22} {:marked? true :number 13}
+                 {:marked? false, :number 8} {:marked? false :number 2}]]
+      (is (true? (bingo? board)))))
 
   (testing "column complete"
-    (let [board [[{:marked true, :value 22} {:marked false :value 13}]
-                 [{:marked true, :value 8} {:marked false :value 2}]
-                 [{:marked true, :value 21} {:marked false :value 9}]
-                 [{:marked true, :value 6} {:marked false :value 10}]
-                 [{:marked true, :value 1} {:marked false :value 12}]]]
+    (let [board [{:marked? true, :number 22} {:marked? false :number 13}
+                 {:marked? true, :number 8} {:marked? false :number 2}]]
       (is (true? (bingo? board))))))
+
+
+(deftest total-bingo-board-test
+  (let [board [{:marked? true, :number 22} {:marked? false :number 13}
+               {:marked? true, :number 8} {:marked? false :number 2}]]
+    (is (= 15
+           (total board)))))
