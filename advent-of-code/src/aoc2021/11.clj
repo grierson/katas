7ea63454f17c8586a5ed3e5b10e9a58cca7cd590
Kyle Grierson <grierson@openmailbox.org>
2022-04-23 01:05:48 +0100

    aoc 2021 day 11 manage state transfers

--- advent-of-code/src/aoc2021/11.clj
+++ advent-of-code/src/aoc2021/11.clj
@@ -1,1 +1,72 @@
 (ns aoc2021.11)
+
+(require 'hashp.core)
+
+(defn recharge [grid] (update-vals grid inc))
+
+(defn neighbours [[bx by] [x y]]
+  (remove
+    (fn [[x y]] (or (> x bx)
+                    (> y by)))
+    (remove (fn [[x y]] (or (neg? x) (neg? y)))
+            [[(dec x) (dec y)] [(dec x) y] [(dec x) (inc y)]
+             [x (dec y)] [x (inc y)]
+             [(inc x) (dec y)] [(inc x) y] [(inc x) (inc y)]])))
+
+
+(defn charge [grid neighbour]
+  (if (zero? (get grid neighbour))
+    grid
+    (update grid neighbour inc)))
+
+(defn flashers [grid]
+  (map first (filter (fn [[_ charge]] (> charge 9)) grid)))
+
+(defn flash [boundary grid octopus]
+  (reduce charge (assoc grid octopus 0) (neighbours boundary octopus)))
+
+(defn step [grid boundary]
+  (let [grid (recharge grid)]
+    (loop [grid grid]
+      (let [flashers (flashers grid)]
+        (if (empty? flashers)
+          grid
+          (recur (reduce (partial flash boundary) grid flashers)))))))
+
+(defn run [grid boundary times]
+  (reduce
+    (fn [state _] (step state boundary))
+    grid
+    (range times)))
+
+(def input ["4738615556"
+            "6744423741"
+            "2812868827"
+            "8844365624"
+            "4546674266"
+            "4518674278"
+            "7457237431"
+            "4524873247"
+            "3153341314"
+            "3721414667"])
+
+
+(defn make-grid
+  [x y]
+  (let [keys (for [x (range x)
+                   y (range y)]
+               [x y])]
+    (apply hash-map (interleave keys (repeat 0)))))
+
+(defn str->grid [s]
+  (let [size (count s)
+        grid (make-grid size size)]
+    (reduce (fn [grid position]
+              (let [value (parse-long (str (get-in s position)))]
+                (assoc grid position value)))
+            grid
+            (for [x (range size)
+                  y (range size)]
+              [x y]))))
+
+;(run (str->grid input) [9 9] 100)
--- advent-of-code/test/aoc2021/11_test.clj
+++ advent-of-code/test/aoc2021/11_test.clj
@@ -1,42 +1,149 @@
 (ns aoc2021.11-test
-  (:require [clojure.test :refer :all])
-  (:require [aoc2021.11 :refer :all]))
+  (:require [clojure.test :refer :all]
+            [aoc2021.11 :refer :all]))
 
-; Each step increase by 1
-; > 9 causes flash
-; - Adjacent octo inc by 1
-; - Reset to 0
+(deftest recharge-test
+  (is (= {[0 0] 1 [0 1] 1
+          [1 0] 1 [1 1] 1}
+         (recharge (make-grid 2 2)))))
 
-; INIT
-; 9 9 9
-; 9 1 9
-; 9 9 9
+(deftest neighbours-test
+  (testing "no negatives"
+    (is (= [[0 1] [1 0] [1 1]]
+           (neighbours [1 1] [0 0]))))
+  (testing "no pass boundary"
+    (is (= [[0 0] [0 1] [1 0]]
+           (neighbours [1 1] [1 1])))))
 
-; 1st step
-; 10 9 9
-; 9  1 9
-; 9  9 9
+(deftest charge-test
+  (testing "Top left flashes"
+    (let [grid (assoc
+                 (make-grid 2 2)
+                 [0 1] 1
+                 [1 0] 1
+                 [1 1] 1)]
 
-; 2nd step
-; 10 10 9
-; 9  1 9
-; 9  9 9
+      (is (= {[0 0] 0
+              [0 1] 2
+              [1 0] 1
+              [1 1] 1}
+             (charge grid [0 1]))))
+    (testing "Don't charge flashed"
+      (is (= {[0 0] 0
+              [0 1] 0
+              [1 0] 0
+              [1 1] 0}
+             (charge (make-grid 2 2) [0 1]))))))
 
-; ...
+(deftest flashers-test
+  (testing "top left is a flasher"
+    (let [grid (make-grid 2 2)
+          grid (assoc grid [0 0] 10)]
+      (is (= [[0 0]]
+             (flashers grid))))
+    (testing "all flashers"
+      (let [grid (make-grid 2 2)
+            grid (assoc grid [0 0] 10
+                             [0 1] 10
+                             [1 0] 10
+                             [1 1] 10)]
+        (is (= (set [[0 0] [0 1] [1 0] [1 1]])
+               (set (flashers grid))))))))
 
-; Last step
-; 10 10 10
-; 10  2 10
-; 10 10 10
-
-(defn step [grid]
-  grid)
-
+(deftest flash-test
+  (testing "top left flashes"
+    (let [grid (assoc
+                 (make-grid 2 2)
+                 [0 0] 10
+                 [0 1] 1
+                 [1 0] 1
+                 [1 1] 1)]
+      (is (= {[0 0] 0
+              [0 1] 2
+              [1 0] 2
+              [1 1] 2}
+             (flash [1 1] grid [0 0]))))))
 
 (deftest step-test
-  (is (= [[10 9 9]
-          [9 1 1]
-          [9 9 9]]
-         (step [[9 9 9]
-                [9 1 9]
-                [9 9 9]]))))
+  (testing "smaller sample step"
+    (let [init (str->grid ["11111"
+                           "19991"
+                           "19191"
+                           "19991"
+                           "11111"])
+          step1 (str->grid ["34543"
+                            "40004"
+                            "50005"
+                            "40004"
+                            "34543"])
+          step2 (str->grid ["45654"
+                            "51115"
+                            "61116"
+                            "51115"
+                            "45654"])]
+      (is (= step1
+             (step init [4 4])))
+      (is (= step2
+             (step step1 [4 4]))))))
+
+(deftest bigger-step-test
+  (testing "bigger sample step"
+    (let [init (str->grid ["5483143223"
+                           "2745854711"
+                           "5264556173"
+                           "6141336146"
+                           "6357385478"
+                           "4167524645"
+                           "2176841721"
+                           "6882881134"
+                           "4846848554"
+                           "5283751526"])
+          step1 (str->grid ["6594254334"
+                            "3856965822"
+                            "6375667284"
+                            "7252447257"
+                            "7468496589"
+                            "5278635756"
+                            "3287952832"
+                            "7993992245"
+                            "5957959665"
+                            "6394862637"])
+          step2 (str->grid ["8807476555"
+                            "5089087054"
+                            "8597889608"
+                            "8485769600"
+                            "8700908800"
+                            "6600088989"
+                            "6800005943"
+                            "0000007456"
+                            "9000000876"
+                            "8700006848"])]
+      (is (= step1
+             (step init [9 9])))
+      (is (= step2
+             (step step1 [9 9]))))))
+
+(deftest sample-100-test
+  (testing "sample to 100 steps"
+    (let [init (str->grid ["5483143223"
+                           "2745854711"
+                           "5264556173"
+                           "6141336146"
+                           "6357385478"
+                           "4167524645"
+                           "2176841721"
+                           "6882881134"
+                           "4846848554"
+                           "5283751526"])
+          step100 (str->grid ["0397666866"
+                              "0749766918"
+                              "0053976933"
+                              "0004297822"
+                              "0004229892"
+                              "0053222877"
+                              "0532222966"
+                              "9322228966"
+                              "7922286866"
+                              "6789998766"])]
+      (is (= step100
+             (run init [9 9] 100))))))
