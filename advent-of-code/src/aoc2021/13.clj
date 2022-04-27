6abdbfb17c9c5aee3ac214c4ce5ef881af91937c
Kyle Grierson <grierson@openmailbox.org>
2022-04-27 20:01:20 +0100

    aoc 2021 day 13 - passing example

--- advent-of-code/src/aoc2021/13.clj
+++ advent-of-code/src/aoc2021/13.clj
@@ -20,13 +20,11 @@
         (parse-dots))))
 
 (defn parse [paper]
-  (update
-    (set/rename-keys
-      (group-by (fn [[a _]] (keyword? a)) (map parse-line paper))
-      {true  :folds
-       false :dots})
-    :dots
-    set))
+  (let [lines (group-by (fn [[a _]] (keyword? a)) (map parse-line paper))]
+    (-> lines
+        (set/rename-keys {true  :folds
+                          false :dots})
+        (update :dots set))))
 
 (defn get-y-halfs [line dots]
   (update-vals
@@ -36,18 +34,33 @@
        false :top})
     set))
 
-(defn relocate-y-dot [fold [x y]]
-  (let [a (- y fold)
-        b (- fold a)]
+(defn get-x-halfs [column dots]
+  (update-vals
+    (set/rename-keys
+      (group-by (fn [[x _]] (>= x column)) dots)
+      {true  :right
+       false :left})
+    set))
+
+(defn relocate-y-dot [line [x y]]
+  (let [a (- y line)
+        b (- line a)]
     [x b]))
 
-(defn relocate-dots [[direction line :as fold] dots]
-  (if (= direction :y)
-    (let [{:keys [top bottom]} (get-y-halfs line dots)]
-      (into top
-            (map (partial relocate-y-dot line) bottom)))))
+(defn relocate-x-dot [column [x y]]
+  (let [a (- x column)
+        b (- column a)]
+    [b y]))
 
-(defn apply-fold [{:keys [dots folds]}]
-  (let [[fold & folds] folds]
-    {:dots  (relocate-dots fold dots)
-     :folds folds}))
+(defn apply-fold [[direction amount] dots]
+  (if (= direction :y)
+    (let [{:keys [top bottom]} (get-y-halfs amount dots)]
+      (into top
+            (map (partial relocate-y-dot amount) bottom)))
+    (let [{:keys [left right]} (get-x-halfs amount dots)]
+      (into left
+            (map (partial relocate-x-dot amount) right)))))
+
+
+(defn execute [{:keys [dots folds]}]
+  (reduce (fn [state fold] (apply-fold fold state)) dots folds))
--- advent-of-code/test/aoc2021/13_test.clj
+++ advent-of-code/test/aoc2021/13_test.clj
@@ -50,25 +50,38 @@
 
 
 
-(deftest get-bottom-half-test
+(deftest get-y-half-test
   (let [{:keys [dots]} (parse example)]
     (is (= #{[6 10]
-             [0, 14]
-             [9, 10]
-             [4, 11]
-             [6, 12]
-             [0, 13]
-             [10, 12]
-             [1, 10]
-             [2, 14]
-             [8, 10]}
+             [0 14]
+             [9 10]
+             [4 11]
+             [6 12]
+             [0 13]
+             [10 12]
+             [1 10]
+             [2 14]
+             [8 10]}
            (:bottom (get-y-halfs 7 dots))))))
 
 
+
+(deftest get-x-half-test
+  (let [dots (apply-fold [:y 7] (:dots (parse example)))]
+    (is (= #{[6 0]
+             [9 0]
+             [6 2]
+             [10 2]
+             [6 4]
+             [8 4]
+             [9 4]
+             [10 4]}
+           (:right (get-x-halfs 5 dots))))))
+
 (deftest relocate-y-dot-test
   (testing "example with a bottom fold at line 7"
-    (are [current y]
-      (= y (relocate-y-dot 7 current))
+    (are [current-location new-location]
+      (= new-location (relocate-y-dot 7 current-location))
       [6 10] [6 4]
       [0 14] [0 0]
       [9 10] [9 4]
@@ -80,24 +93,74 @@
       [2 14] [2 0]
       [8 10] [8 4])))
 
+(deftest relocate-x-dot-test
+  (testing "example with a right fold at column 5"
+    (are [current-location new-location]
+      (= new-location (relocate-x-dot 5 current-location))
+      [6 0] [4 0]
+      [9 0] [1 0]
+      [6 2] [4 2])))
+
 
 (deftest apply-fold-test
-  (let [{:keys [dots folds]} (apply-fold (parse example))]
-    (is (= dots #{[6 4]
-                  [0 0]
-                  [9 4]
-                  [0 3]
-                  [10 4]
-                  [4 3]
-                  [6 0]
-                  [6 2]
-                  [4 1]
-                  [0 1]
-                  [10 2]
-                  [3 4]
-                  [3 0]
-                  [1 4]
-                  [2 0]
-                  [8 4]
-                  [9 0]}))
-    (is (= folds [[:x 5]]))))
+  (testing "first example bottom fold"
+    (let [dots (apply-fold [:y 7] (:dots (parse example)))]
+      (is (= #{[6 4]
+               [0 0]
+               [9 4]
+               [0 3]
+               [10 4]
+               [4 3]
+               [6 0]
+               [6 2]
+               [4 1]
+               [0 1]
+               [10 2]
+               [3 4]
+               [3 0]
+               [1 4]
+               [2 0]
+               [8 4]
+               [9 0]}
+             dots))))
+  (testing "second example right fold"
+    (let [dots (apply-fold [:x 5] (apply-fold [:y 7] (:dots (parse example))))]
+      (is (= #{[0 0]
+               [0 1]
+               [0 2]
+               [0 3]
+               [0 4]
+               [1 0]
+               [1 4]
+               [2 0]
+               [2 4]
+               [3 0]
+               [3 4]
+               [4 0]
+               [4 1]
+               [4 2]
+               [4 3]
+               [4 4]}
+             dots)))))
+
+
+(deftest execute-test
+  (testing "execute example"
+    (let [dots (execute (parse example))]
+      (is (= #{[0 0]
+               [0 1]
+               [0 2]
+               [0 3]
+               [0 4]
+               [1 0]
+               [1 4]
+               [2 0]
+               [2 4]
+               [3 0]
+               [3 4]
+               [4 0]
+               [4 1]
+               [4 2]
+               [4 3]
+               [4 4]}
+             dots)))))
