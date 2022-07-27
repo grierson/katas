48a824dcb8df30116b598bebfbff8ec48be6aa9b
Kyle Grierson <griersonky@gmail.com>
2022-07-27 22:31:53 +0100

    day2 part 2

--- advent-of-code/src/aoc2017/02.clj
+++ advent-of-code/src/aoc2017/02.clj
@@ -8,12 +8,30 @@
 (defn make-low-high-pair [nums]
   [(apply min nums) (apply max nums)])
 
-(defn solve1 [lines]
-  (->> (str/split-lines lines)
-       (map parse-line)
-       (map make-low-high-pair)
-       (map (fn [[low high]] (- high low)))
-       (reduce +)))
+(defn make-divisible-pair [nums]
+  (defn foo [nums [x & xs]]
+    (when x
+      (let [a (some (fn [y]
+                      (when (not= x y)
+                        (cond
+                          (int? (/ x y)) [x y]
+                          (int? (/ y x)) [y x]))) nums)]
+        (if a
+          a
+          (foo nums xs)))))
+  (foo nums (set nums)))
+
+(defn solve-fn [pair-fn total-fn]
+  (fn  [lines]
+    (->> (str/split-lines lines)
+         (map parse-line)
+         (map pair-fn)
+         (map total-fn)
+         (reduce +))))
+
+(def solve1 (solve-fn make-low-high-pair (fn [[low high]] (- high low))))
+(def solve2 (solve-fn make-divisible-pair (fn [[high low]] (/ high low))))
 
 (comment
-  (solve1 (slurp (io/resource "aoc2017/02.txt"))))
+  (solve1 (slurp (io/resource "aoc2017/02.txt")))
+  (solve2 (slurp (io/resource "aoc2017/02.txt"))))
--- advent-of-code/test/aoc2017/02_test.clj
+++ advent-of-code/test/aoc2017/02_test.clj
@@ -1,7 +1,8 @@
 (ns aoc2017.02-test
   (:require
-   [aoc2017.02 :refer [solve1 make-low-high-pair parse-line]]
-   [clojure.test :refer [are deftest is]]))
+   [aoc2017.02 :refer [make-divisible-pair make-low-high-pair parse-line
+                       solve1 solve2]]
+   [clojure.test :refer [deftest is]]))
 
 (def sample1 "5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8")
 
@@ -24,3 +25,17 @@
 (deftest sample1-test
   (is (= 18
          (solve1 sample1))))
+
+(deftest make-divisable-pair-test
+  (is (= [8 2]
+         (make-divisible-pair [5 9 2 8])))
+  (is (= [9 3]
+         (make-divisible-pair [9 4 7 3])))
+  (is (= [6 3]
+         (make-divisible-pair [3 8 6 5]))))
+
+(def sample2 "5\t9\t2\t8\n9\t4\t7\t3\t\n3\t8\t6\t5")
+
+(deftest sample2-test
+  (is (= 9
+         (solve2 sample2))))
