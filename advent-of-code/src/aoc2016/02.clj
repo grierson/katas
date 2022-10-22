71bf88d94a33c359244e0f84859f3a9fa763e593
Kyle Grierson <griersonky@gmail.com>
2022-10-22 19:46:54 +0100

    2016 - day 2 - part 2

--- advent-of-code/src/aoc2016/02.clj
+++ advent-of-code/src/aoc2016/02.clj
@@ -9,17 +9,59 @@
     \U (if (#{1 2 3} position) position (- position 3))
     \D (if (#{7 8 9} position) position (+ position 3))))
 
-(defn get-number [position [h & t]]
-  (if h
-    (recur (move position h) t)
-    position))
+(defn get-number [position directions]
+  (reduce move position directions))
 
-(defn solve [lines]
-  (apply str
-         (reduce
-          (fn [state instructions] (conj state (get-number (last state) instructions)))
-          [(get-number 5 (first lines))]
-          (rest lines))))
+(defn- update-state [state instructions]
+  (conj state (get-number (last state) instructions)))
+
+(defn solve
+  [lines]
+  (apply
+   str
+   (reduce
+    update-state
+    [(get-number 5 (first lines))]
+    (rest lines))))
+
+(def diamond-keypad
+  [[nil nil 1 nil nil]
+   [nil 2 3 4 nil]
+   [5 6 7 8 9]
+   [nil \A \B \C nil]
+   [nil nil \D nil nil]])
+
+(defn find-number [coords]
+  (get-in diamond-keypad coords))
+
+(defn get-coord [[row column] direction]
+  (case direction
+    \U [(- row 1) column]
+    \D [(+ row 1) column]
+    \L [row (- column 1)]
+    \R [row (+ column 1)]))
+
+(defn valid-move? [location direction]
+  (let [coord (get-coord location direction)]
+    (when (find-number coord)
+      coord)))
+
+(defn trace-moves [state instruction]
+  (if-let [new-coord (valid-move? (last state) instruction)]
+    (conj state new-coord)
+    state))
+
+(defn apply-moves [location instructions]
+  (last (reduce trace-moves (vector location) instructions)))
+
+(defn solve2
+  [lines]
+  (apply
+   str
+   (reduce
+    apply-moves
+    [(apply-moves [2 0] (first lines))]
+    (rest lines))))
 
 (comment
   (def lines (str/split-lines (slurp (io/resource "aoc2016/02.txt"))))
--- advent-of-code/test/aoc2016/02_test.clj
+++ advent-of-code/test/aoc2016/02_test.clj
@@ -1,6 +1,12 @@
 (ns aoc2016.02-test
   (:require
-   [aoc2016.02 :refer [get-number solve]]
+   [aoc2016.02 :refer [find-number
+                       solve
+                       get-coord
+                       get-number
+                       trace-moves
+                       valid-move?
+                       apply-moves]]
    [clojure.test :refer [are deftest is testing]]))
 
 (deftest get-number-test
@@ -16,3 +22,31 @@
 
 (deftest solve-test
   (is (= (solve ["ULL" "RRDDD" "LURDL" "UUUD"]) "1985")))
+
+(deftest get-location-test
+  (are [actual expected] (= (find-number actual) expected)
+    [0 0] nil
+    [2 0] 5
+    [2 2] 7
+    [0 2] 1))
+
+(deftest get-coord-test
+  (testing "get next coord"
+    (is (= [1 0] (get-coord [2 0] \U)))
+    (is (= [3 0] (get-coord [2 0] \D)))
+    (is (= [2 -1] (get-coord [2 0] \L)))
+    (is (= [2 1] (get-coord [2 0] \R)))))
+
+(deftest get-next-coord-test
+  (is (= nil (valid-move? [2 0] \L)))
+  (is (= [2 1] (valid-move? [2 0] \R))))
+
+(deftest update-state2-test
+  (is (= (trace-moves [[2 0]] \U) [[2 0]]))
+  (is (= (trace-moves [[2 0]] \R) [[2 0] [2 1]])))
+
+(deftest apply-moves-test
+  (is (= (apply-moves [2 0] "ULL") [2 0]))
+  (is (= (apply-moves [2 0] "RRDDD") [4 2]))
+  (is (= (apply-moves [4 2] "LURDL") [3 2]))
+  (is (= (apply-moves [3 2] "UUUUD") [1 2])))
