9a3fb8d2491107a6bb904e418d8b9f8ba74bfa9f
Kyle Grierson <griersonky@gmail.com>
2022-10-22 20:30:39 +0100

    2016 - day 3 - part 2

--- advent-of-code/src/aoc2016/03.clj
+++ advent-of-code/src/aoc2016/03.clj
@@ -7,11 +7,34 @@
        (< a (+ b c))
        (< b (+ c a))))
 
-
 (defn parse-line [line]
-  ((comp (fn [nums] (map #(Integer/parseInt %) nums)) #(re-seq #"\d+" %)) line))
+  ((comp (fn [nums] (map parse-long nums)) #(re-seq #"\d+" %)) line))
+
+(defn- parse-lines [lines]
+  (map parse-line lines))
+
+(defn- parse-lines2 [lines]
+  (let [lines (map parse-line lines)]
+    (->> (partition 3 lines)
+         (map (fn [lines]
+                [(map first lines)
+                 (map second lines)
+                 (map #(nth % 2) lines)]))
+         (apply concat))))
+
+(defn count-valid [lines]
+  (count (filter valid? lines)))
+
+(defn solve1 [data]
+  (count-valid (parse-lines data)))
+
+(defn solve2 [data]
+  (count-valid (parse-lines2 data)))
 
 (comment
   (def lines (str/split-lines (slurp (io/resource "aoc2016/03.txt"))))
-  (count (filter valid? (map parse-line lines))))
+  (solve1 lines)
+  (solve2 lines)
+  (parse-lines lines)
+  (parse-lines2 lines))
 
