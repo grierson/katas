b529e161c5a390800f48d0f0a2d087a9dfc62ca9
Kyle Grierson <griersonky@gmail.com>
2022-10-23 15:26:28 +0100

    2016 day 5 part 2

--- advent-of-code/src/aoc2016/05.clj
+++ advent-of-code/src/aoc2016/05.clj
@@ -27,7 +27,42 @@
   (let [hashs (finder secret)]
     (apply str (map #(nth % 5) hashs))))
 
+; ---
+
+(defn update-password [s idx replacement]
+  (if (= \_ (get s idx))
+    (str (subs s 0 idx) replacement (subs s (inc idx)))
+    s))
+
+(defn valid2? [hash]
+  (and
+   (str/starts-with? hash "00000")))
+
+(defn password-complete? [password]
+  (not (some? (some #{\_} password))))
+
+(defn append-valid-hash2 [password id]
+  (let [hash (digest/md5 id)]
+    (if (valid2? hash)
+      (let [index (parse-long (str (nth hash 5)))
+            value (nth hash 6)]
+        (update-password password index value))
+      password)))
+
+(defn found-all-hashes2
+  [password id]
+  (if (password-complete? password)
+    (reduced password)
+    (append-valid-hash2 password id)))
+
+(defn solve2 [secret]
+  (reduce
+   found-all-hashes2
+   "________"
+   (lazy-seq (map #(str secret %) (range)))))
+
 (comment
   (solve "abc")
-  (finder "reyedfim")
-  (solve "reyedfim"))
+  (solve2 "abc")
+  (solve "reyedfim")
+  (solve2 "reyedfim"))
--- advent-of-code/test/aoc2016/05_test.clj
+++ advent-of-code/test/aoc2016/05_test.clj
@@ -1,9 +1,12 @@
 (ns aoc2016.05-test
   (:require
-   [aoc2016.05 :refer [solve
-                       append-valid-hash
+   [aoc2016.05 :refer [append-valid-hash
+                       append-valid-hash2
+                       password-complete?
+                       update-password
+                       solve
                        valid?]]
-   [clojure.test :refer [deftest is are]]))
+   [clojure.test :refer [are deftest is testing]]))
 
 (deftest valid?-test
   (is (false? (valid? "0000abc")))
@@ -16,6 +19,24 @@
     "abc3231929" \1
     "abc5017308" \8
     "abc5278568" \f))
+
+(deftest update-password-test
+  (testing "do not replace non \\_ values"
+    (is (= "1____" (update-password "_____" 0 \1))))
+  (testing "do not replace non \\_ values"
+    (is (= "1____" (update-password "1____" 0 \x)))))
+
+(deftest password-complete-test
+  (is (false? (password-complete? "12_")))
+  (is (true? (password-complete? "123"))))
+
+(deftest append-valid-hash2-test
+  (are [state id expected]
+       (= (append-valid-hash2 state id)
+          expected)
+    "________" "abc3231929" "_5______"
+    "_5______" "abc5017308" "_5______"
+    "_5______" "abc5357525" "_5__e___"))
 
 (comment
   (deftest find-hash-test
