2a18dfb4b0c0a5cbce4e5d4d12b0da68ed703b84
Kyle Grierson <griersonky@gmail.com>
2022-10-23 17:15:19 +0100

    2016 - day 6 - refactor

--- advent-of-code/test/aoc2016/06_test.clj
+++ advent-of-code/test/aoc2016/06_test.clj
@@ -1,7 +1,9 @@
 (ns aoc2016.06-test
   (:require
    [aoc2016.06 :refer [most-common
-                       get-letters]]
+                       least-common
+                       get-letters
+                       solve]]
    [clojure.test :refer [deftest is]]))
 
 (def sample ["eedadn"
@@ -21,13 +23,20 @@
              "dvrsen"
              "enarar"])
 
-
 (deftest get-letters-test
-  (is (=  "ederatsrnnstvvde" (get-letters sample 0)))
-  (is (=  "eraatsdastvenrvn" (get-letters sample 1)))
-  (is (=  "dvnaertssnestdra" (get-letters sample 2))))
+  (is (= "ederatsrnnstvvde" (get-letters sample 0)))
+  (is (= "eraatsdastvenrvn" (get-letters sample 1)))
+  (is (= "dvnaertssnestdra" (get-letters sample 2))))
 
 (deftest most-common-test
   (is (= \e (most-common "ederatsrnnstvvde")))
   (is (= \a (most-common "eraatsdastvenrvn")))
   (is (= \s (most-common "dvnaertssnestdra"))))
+
+(deftest least-common-test
+  (is (= \a (least-common "ederatsrnnstvvde")))
+  (is (= \d (least-common "eraatsdastvenrvn")))
+  (is (= \v (least-common "dvnaertssnestdra"))))
+
+(deftest solve-test
+  (is (= "easter" (solve sample))))
