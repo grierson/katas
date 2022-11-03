37ac52cbc03802d09a98fa76e712689797fa7f56
Kyle Grierson <griersonky@gmail.com>
2022-11-03 03:25:50 +0000

    Matching outside space

--- diamond/test/diamond/core_test.clj
+++ diamond/test/diamond/core_test.clj
@@ -14,11 +14,10 @@
    [letter (s/gen letter?)]
    (false? (string/blank? (diamond letter)))))
 
-
 (defspec first-row-contains-A
   (prop/for-all
    [letter (s/gen letter?)]
-   (= "A" 
+   (= "A"
       (-> (diamond letter)
           (string/split-lines)
           (first)
@@ -27,8 +26,23 @@
 (defspec last-row-contains-A
   (prop/for-all
    [letter (s/gen letter?)]
-   (= "A" 
+   (= "A"
       (-> (diamond letter)
           (string/split-lines)
           (last)
           (string/trim)))))
+
+(defspec each-row-has-matching-outside-space
+  (prop/for-all
+   [letter (s/gen letter?)]
+   (every?
+    true?
+    (map
+     (fn [line]
+       (let [letter (first (string/trim line))
+             first-letter (string/index-of line letter)
+             second-letter (string/last-index-of line letter)
+             leading-space (subs line 0 first-letter)
+             trailing-space (subs line (inc second-letter))]
+         (= leading-space trailing-space)))
+     (string/split-lines (diamond letter))))))
