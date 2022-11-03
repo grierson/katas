f06ebf2376195c4c7b11deba818e95a733d3359d
Kyle Grierson <griersonky@gmail.com>
2022-11-03 19:35:07 +0000

    Refactor

--- diamond/src/diamond/core.clj
+++ diamond/src/diamond/core.clj
@@ -1,7 +1,5 @@
 (ns diamond.core
-  (:require
-   [clojure.spec.alpha :as s]
-   [clojure.string :as string]))
+  (:require [clojure.string :as string]))
 
 (def alphabet "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 (def letter? (set alphabet))
@@ -10,24 +8,28 @@
   (let [index (.indexOf (vec letter?) letter)]
     (take (inc index) letter?)))
 
-(defn make-line [width [letter letter-idx]]
+(defn spaces [n]
+  (apply str (repeat n " ")))
+
+(defn a-line-spacing [width]
+  (let [half (/ (dec width) 2)
+        padding (spaces half)]
+    (str padding \A padding)))
+
+(defn letter-line-spacing [width [letter-idx letter]]
+  (let [inner-space-width (dec (* letter-idx 2))
+        padding-width (/ (- width 2 inner-space-width) 2)
+        padding (spaces padding-width)
+        inner-space (spaces inner-space-width)]
+    (str padding letter inner-space letter padding)))
+
+(defn make-line [width [_ letter :as indexed-letter]]
   (if (= letter \A)
-    (let [half (/ (dec width) 2)
-          padding (apply str (repeat half " "))]
-      (str padding letter padding))
-    (let [inner-space-width (dec (* letter-idx 2))
-          padding (apply str (repeat (/ (- width 2 inner-space-width) 2) " "))
-          inner-space (apply str (repeat inner-space-width " "))]
-      (str padding letter inner-space letter padding))))
+    (a-line-spacing width)
+    (letter-line-spacing width indexed-letter)))
 
 (defn make [letter]
-  (let [letters
-        (map-indexed
-         (fn [index letter] [letter index])
-         (range-letters letter))
-
+  (let [letters (map-indexed vector (range-letters letter))
         letters (concat letters (rest (reverse letters)))
         width (count letters)]
     (string/join "\n" (map (partial make-line width) letters))))
-
-(make \D)
