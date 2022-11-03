7ca3e517dedcd157f9d5d89827a2e8015b9c8233
Kyle Grierson <griersonky@gmail.com>
2022-11-03 04:02:18 +0000

    in order

--- diamond/src/diamond/core.clj
+++ diamond/src/diamond/core.clj
@@ -1,6 +1,17 @@
 (ns diamond.core
   (:require [clojure.spec.alpha :as s]))
 
+(def alphabet "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+(def letter? (set alphabet))
 
-(defn diamond [letter]
-  "  A  ")
+(defn range-letters [letter]
+  (let [index (.indexOf (vec letter?) letter)]
+    (take (inc index) letter?)))
+
+(defn make [letter]
+  (let [letters (range-letters letter)
+        reverse-letters (rest (reverse letters))
+        letters (concat letters reverse-letters)]
+    (reduce
+     (fn [state c] (str state "\n" c))
+     (map str letters))))
--- diamond/test/diamond/core_test.clj
+++ diamond/test/diamond/core_test.clj
@@ -4,45 +4,62 @@
    [clojure.test.check.properties :as prop]
    [clojure.spec.alpha :as s]
    [clojure.string :as string]
-   [diamond.core :refer [diamond]]))
-
-(def alphabet "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-(def letter? (set alphabet))
+   [diamond.core :refer [make letter? range-letters]]))
 
 (defspec string-always-returned
   (prop/for-all
    [letter (s/gen letter?)]
-   (false? (string/blank? (diamond letter)))))
+   (let [diamond (make letter)]
+     (false? (string/blank? diamond)))))
 
 (defspec first-row-contains-A
   (prop/for-all
    [letter (s/gen letter?)]
-   (= "A"
-      (-> (diamond letter)
-          (string/split-lines)
-          (first)
-          (string/trim)))))
+   (let [diamond (make letter)]
+     (= "A"
+        (-> diamond
+            (string/split-lines)
+            (first)
+            (string/trim))))))
 
 (defspec last-row-contains-A
   (prop/for-all
    [letter (s/gen letter?)]
-   (= "A"
-      (-> (diamond letter)
-          (string/split-lines)
-          (last)
-          (string/trim)))))
+   (let [diamond (make letter)]
+     (= "A"
+        (-> diamond
+            (string/split-lines)
+            (last)
+            (string/trim))))))
 
 (defspec each-row-has-matching-outside-space
   (prop/for-all
    [letter (s/gen letter?)]
-   (every?
-    true?
-    (map
-     (fn [line]
-       (let [letter (first (string/trim line))
-             first-letter (string/index-of line letter)
-             second-letter (string/last-index-of line letter)
-             leading-space (subs line 0 first-letter)
-             trailing-space (subs line (inc second-letter))]
-         (= leading-space trailing-space)))
-     (string/split-lines (diamond letter))))))
+   (let [diamond (make letter)]
+     (every?
+      true?
+      (map
+       (fn [line]
+         (let [line-letter (first (string/trim line))
+               first-letter (string/index-of line line-letter)
+               second-letter (string/last-index-of line line-letter)
+               leading-space (subs line 0 first-letter)
+               trailing-space (subs line (inc second-letter))]
+           (= leading-space trailing-space)))
+       (string/split-lines diamond))))))
+
+(defspec all-rows-in-order
+  (prop/for-all
+   [letter (s/gen letter?)]
+   (let [diamond (make letter)
+         letters (range-letters letter)
+         reverse-letters (rest (reverse letters))
+         expected-letters (concat letters reverse-letters)
+         actual (->> (string/split-lines diamond)
+                     (map string/trim)
+                     (map first))]
+     (= expected-letters
+        actual))))
+
+
+
