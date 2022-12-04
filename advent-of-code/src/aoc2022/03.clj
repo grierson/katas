a739b429b9829df743ba5296ec4cc4c7be1c032d
Kyle Grierson <griersonky@gmail.com>
2022-12-04 14:05:53 +0000

    Day 3 refactor

--- advent-of-code/src/aoc2022/03.clj
+++ advent-of-code/src/aoc2022/03.clj
@@ -17,31 +17,24 @@ CrZsJsPPZsGzwwsLwLmpwMDw")
 (defn find-duplicate [sections]
   (first (apply set/intersection (map set sections))))
 
-(defn duplicates [rucksack]
+(defn split-rucksack [rucksack]
   (let [l (count rucksack)
-        half (/ l 2)
-        sections [(subs rucksack 0 half) (subs rucksack half)]]
-    (find-duplicate sections)))
-
-(def duplicates2 find-duplicate)
+        half (/ l 2)]
+    [(subs rucksack 0 half) (subs rucksack half)]))
 
 (def alphabet "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 (def priority (into {} (map vector alphabet (range 1 (inc (count alphabet))))))
 
-(defn solve [rucksacks]
+(defn solve-fn [split-fn rucksacks]
   (->> rucksacks
        str/split-lines
-       (map duplicates)
-       (map priority)
-       (reduce +)))
-
-(defn solve2 [rucksacks]
-  (->> rucksacks
-       str/split-lines
-       (partition 3)
+       split-fn
        (map find-duplicate)
        (map priority)
        (reduce +)))
+
+(def solve (partial solve-fn (fn [coll] (map split-rucksack coll))))
+(def solve2 (partial solve-fn (fn [coll] (partition 3 coll))))
 
 (comment
   (solve sample)
