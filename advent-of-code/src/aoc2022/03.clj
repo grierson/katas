fc9b24961295b4b7f153fc0678beb80c4eee8776
Kyle Grierson <griersonky@gmail.com>
2022-12-04 14:04:55 +0000

    Refactor day 4

--- advent-of-code/src/aoc2022/03.clj
+++ advent-of-code/src/aoc2022/03.clj
@@ -14,19 +14,37 @@ CrZsJsPPZsGzwwsLwLmpwMDw")
 
 (def data (slurp (io/resource "aoc2022/03.txt")))
 
+(defn find-duplicate [sections]
+  (first (apply set/intersection (map set sections))))
+
 (defn duplicates [rucksack]
   (let [l (count rucksack)
         half (/ l 2)
-        [c1 c2] [(subs rucksack 0 half) (subs rucksack half)]]
-    (set/intersection (set c1) (set c2))))
+        sections [(subs rucksack 0 half) (subs rucksack half)]]
+    (find-duplicate sections)))
+
+(def duplicates2 find-duplicate)
 
 (def alphabet "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 (def priority (into {} (map vector alphabet (range 1 (inc (count alphabet))))))
 
-(defn solve [data]
-  (->> data
+(defn solve [rucksacks]
+  (->> rucksacks
        str/split-lines
        (map duplicates)
-       (map first)
        (map priority)
        (reduce +)))
+
+(defn solve2 [rucksacks]
+  (->> rucksacks
+       str/split-lines
+       (partition 3)
+       (map find-duplicate)
+       (map priority)
+       (reduce +)))
+
+(comment
+  (solve sample)
+  (solve data)
+  (solve2 sample)
+  (solve2 data))
--- advent-of-code/test/aoc2022/03_test.clj
+++ advent-of-code/test/aoc2022/03_test.clj
@@ -1,21 +1,35 @@
 (ns aoc2022.03-test
   (:require [clojure.test :refer [deftest is]]
-            [aoc2022.03 :refer [sample duplicates solve]]))
+            [aoc2022.03 :refer [sample
+                                duplicates
+                                solve
+                                find-duplicate
+                                solve2]]))
 
 (deftest duplicates-test
-  (is (= #{\p}
+  (is (= \p
          (duplicates "vJrwpWtwJgWrhcsFMMfFFhFp")))
-  (is (= #{\L}
+  (is (= \L
          (duplicates "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL")))
-  (is (= #{\P}
+  (is (= \P
          (duplicates "PmmdzqPrVvPwwTWBwg")))
-  (is (= #{\v}
+  (is (= \v
          (duplicates "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn")))
-  (is (= #{\t}
+  (is (= \t
          (duplicates "ttgJtRGJQctTZtZT")))
-  (is (= #{\s}
+  (is (= \s
          (duplicates "CrZsJsPPZsGzwwsLwLmpwMDw"))))
+
+(deftest duplicates2-test
+  (is (= \r
+         (find-duplicate ["vJrwpWtwJgWrhcsFMMfFFhFp"
+                          "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+                          "PmmdzqPrVvPwwTWBwg"]))))
 
 (deftest solve-test
   (is (= 157
          (solve sample))))
+
+(deftest solve2-test
+  (is (= 70
+         (solve2 sample))))
