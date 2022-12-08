d2b574a4ad634659904390f6bd37f10a9251e6e9
Kyle Grierson <griersonky@gmail.com>
2022-12-08 00:17:39 +0000

    parse total

--- advent-of-code/src/aoc2022/07.clj
+++ advent-of-code/src/aoc2022/07.clj
@@ -1,5 +1,6 @@
 (ns aoc2022.07
   (:require
+   [clojure.java.io :as io]
    [clojure.string :as str]))
 
 (def sample "$ cd /
@@ -64,10 +65,11 @@ $ ls
     (str/starts-with? cmd "dir") (handle-dir system cmd)
     :else (handle-file system cmd)))
 
+(defn parse [cmds]
+  (dissoc  (reduce run {} cmds) :wd))
+
 (comment
-  (reduce run
-          {}
-          (str/split-lines sample)))
+  (parse (str/split-lines sample)))
 
 (defn folder-size
   [{:keys [files] :as fs}]
@@ -77,17 +79,38 @@ $ ls
             (reduce + (map :size files))
             (map #(folder-size (get fs %)) (keys (dissoc fs :files))))))
 
-(comment
-  (def fs (dissoc (reduce run {} (str/split-lines sample)) :wd))
-  (folder-size fs))
-
-(comment
-  {:total 10
-   :a {:total 5
-       :b {:total 3}}})
-
 (defn total
-  [{:keys [files] :as fs}]
-  (apply merge
-         {:total (folder-size fs)}
-         (map (fn [dir] {dir (total (get fs dir))}) (keys (dissoc fs :files)))))
+  [fs]
+  (let [dirs (keys (dissoc fs :files))
+        dir-total (folder-size fs)]
+    (apply merge
+           {:total dir-total}
+           (map
+            (fn [dir]
+              {dir (total (dir fs))})
+            dirs))))
+
+(comment
+  (def sample-fs (parse (str/split-lines sample)))
+  (total sample-fs))
+
+(defn solve1 [{:keys [total]
+               :or {total 0}
+               :as fs}]
+  (let [ks (keys (dissoc fs :total))]
+    (reduce
+     +
+     (if (< total 100000) total 0)
+     (map #(solve1 (get fs %)) ks))))
+
+(comment
+  (solve1 (total sample-fs)))
+
+(def data (slurp (io/resource "aoc2022/07.txt")))
+
+(comment
+  (def fs (parse (str/split-lines data)))
+  (total fs)
+  (folder-size fs)
+  (solve1 (total fs)))
+
