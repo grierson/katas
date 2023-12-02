44e413b2dad4b08491d87325ad0cc778b9ca6181
Kyle Grierson <griersonky@gmail.com>
2023-12-02 23:30:35 +0000

    day 1 - very tired need to rework this

--- advent-of-code/src/aoc2023/01.clj
+++ advent-of-code/src/aoc2023/01.clj
@@ -2,10 +2,94 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
+(require 'hashp.core)
+
 (def data (slurp (io/resource "aoc2023/01.txt")))
+
+(def ones {"one" 1
+           "two" 2
+           "three" 3
+           "four" 4
+           "five" 5
+           "six" 6
+           "seven" 7
+           "eight" 8
+           "nine" 9})
+
+(def rev-ones {"eno" 1
+               "owt" 2
+               "eerht" 3
+               "ruof" 4
+               "evif" 5
+               "xis" 6
+               "neves" 7
+               "thgie" 8
+               "enin" 9})
 
 (defn parse
   [calibration]
   (let [numbers (filter #(Character/isDigit %) calibration)
         [first last] ((juxt first last) numbers)]
     (parse-long (str first last))))
+
+(defn find-first-digit [s]
+  (loop [index 0]
+    (if (< index (count s))
+      (let [char (get s index)]
+        (if (Character/isDigit char)
+          [index (parse-long (str char))]
+          (recur (inc index))))
+      nil)))
+
+(defn find-first-one [s]
+  (loop [index 0]
+    (if (<= index (count s))
+      (let [remaining (subs s index)]
+        (if-let [one-key (some #(when (.startsWith remaining %) %) (keys ones))]
+          [index (get ones one-key)]
+          (recur (inc index))))
+      nil)))
+
+(defn find-first
+  [calibration]
+  (let [[index-digit value-digit] (find-first-digit calibration)
+        [index-one value-one] (find-first-one calibration)]
+    (cond
+      (nil? index-digit) value-one
+      (nil? index-one) value-digit
+      :else (if (> index-digit index-one) value-one value-digit))))
+
+(defn find-last-one [s]
+  (loop [index 0]
+    (if (<= index (count s))
+      (let [remaining (subs s index)]
+        (if-let [one-key (some #(when (.startsWith remaining %) %) (keys rev-ones))]
+          [index (get rev-ones one-key)]
+          (recur (inc index))))
+      nil)))
+
+(defn find-last
+  [calibration]
+  (let [rev-str (apply str (reverse calibration))
+        [index-digit value-digit] (find-first-digit rev-str)
+        [index-one value-one] (find-last-one rev-str)]
+    (cond
+      (nil? index-digit) value-one
+      (nil? index-one) value-digit
+      :else (if (> index-digit index-one) value-one value-digit))))
+
+(defn parse2
+  [calibration]
+  (let [left (find-first calibration)
+        right (find-last calibration)]
+    (parse-long (str left right))))
+
+(defn solve [input]
+  (reduce + (map parse (str/split-lines input))))
+
+(defn solve2 [input]
+  (reduce + (map parse2 (str/split-lines input))))
+
+(comment
+  (solve data)
+  (solve2 data))
--- advent-of-code/test/aoc2023/01_test.clj
+++ advent-of-code/test/aoc2023/01_test.clj
@@ -1,6 +1,6 @@
 (ns aoc2023.01-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [aoc2023.01 :refer [parse]]))
+  (:require [clojure.test :refer [deftest is testing are]]
+            [aoc2023.01 :refer [parse parse2 find-first find-last]]))
 
 (deftest parse-test
   (testing "first example"
@@ -11,3 +11,33 @@
     (is (= (parse "a1b2c3d4e5f") 15)))
   (testing "fourth example"
     (is (= (parse "treb7uchet") 77))))
+
+(deftest find-first-test
+  (are [x y] (= y (find-first x))
+    "two1nine" 2
+    "eightwothree" 8
+    "abcone2threexyz" 1
+    "xtwone3four" 2
+    "4nineeightseven2" 4
+    "zoneight234" 1
+    "7pqrstsixteen" 7))
+
+(deftest find-last-test
+  (are [x y] (= y (find-last x))
+    "two1nine" 9
+    "eightwothree" 3
+    "abcone2threexyz" 3
+    "xtwone3four" 4
+    "4nineeightseven2" 2
+    "zoneight234" 4
+    "7pqrstsixteen" 6))
+
+(deftest parse2-test
+  (are [x y] (= y (parse2 x))
+    "two1nine" 29
+    "eightwothree" 83
+    "abcone2threexyz" 13
+    "xtwone3four" 24
+    "4nineeightseven2" 42
+    "zoneight234" 14
+    "7pqrstsixteen" 76))
