28e811cdd42afe0de96f3ede266ad34d0f3455f5
Kyle Grierson <griersonky@gmail.com>
2024-12-10 15:19:07 +0000

    2024 - day 4 - part 2

--- advent-of-code/src/aoc2024/04.clj
+++ advent-of-code/src/aoc2024/04.clj
@@ -17,14 +17,19 @@ MXMXAXMASX")
 
 (def input (slurp (io/resource "aoc2024/04.txt")))
 
-(def directions {:north [[-1 0] [-2 0] [-3 0]]
-                 :north-east [[-1 1] [-2 2] [-3 3]]
-                 :east [[0 1] [0 2] [0 3]]
-                 :south-east [[1 1] [2 2] [3 3]]
-                 :south [[1 0] [2 0] [3 0]]
-                 :south-west [[1 -1] [2 -2] [3 -3]]
-                 :west [[0 -1] [0 -2] [0 -3]]
-                 :north-west [[-1 -1] [-2 -2] [-3 -3]]})
+(def mas-directions {:north [[-1 0] [-2 0] [-3 0]]
+                     :north-east [[-1 1] [-2 2] [-3 3]]
+                     :east [[0 1] [0 2] [0 3]]
+                     :south-east [[1 1] [2 2] [3 3]]
+                     :south [[1 0] [2 0] [3 0]]
+                     :south-west [[1 -1] [2 -2] [3 -3]]
+                     :west [[0 -1] [0 -2] [0 -3]]
+                     :north-west [[-1 -1] [-2 -2] [-3 -3]]})
+
+(def xmas-directions {:north-east [-1 1]
+                      :south-east [1 1]
+                      :south-west [1 -1]
+                      :north-west [-1 -1]})
 
 ;; S  S  S
 ;;  A A A
@@ -34,26 +39,36 @@ MXMXAXMASX")
 ;;  A A A
 ;; S  S  S
 
-(defn path [direction [ly lx]]
-  (let [path (get directions direction)]
+(defn path [directions direction [ly lx]]
+  (let [path #p (get directions direction)]
     (map (fn [[y x]] [(+ ly y) (+ lx x)]) path)))
 
-(defn surrounding [location]
-  {:north (path :north location)
-   :north-east (path :north-east location)
-   :east (path :east location)
-   :south-east (path :south-east location)
-   :south (path :south location)
-   :south-west (path :south-west location)
-   :west (path :west location)
-   :north-west (path :north-west location)})
+(defn xmas-path [directions direction [ly lx]]
+  (let [[dy dx] (get directions direction)]
+    [(+ ly dy) (+ lx dx)]))
 
-(defn find-all-x [grid]
+(defn mas-surrounding [location]
+  {:north (path mas-directions :north location)
+   :north-east (path mas-directions  :north-east location)
+   :east (path mas-directions  :east location)
+   :south-east (path mas-directions  :south-east location)
+   :south (path mas-directions  :south location)
+   :south-west (path mas-directions  :south-west location)
+   :west (path mas-directions  :west location)
+   :north-west (path mas-directions  :north-west location)})
+
+(defn xmas-surrounding [location]
+  {:north-east (xmas-path xmas-directions :north-east location)
+   :south-east (xmas-path xmas-directions :south-east location)
+   :south-west (xmas-path xmas-directions :south-west location)
+   :north-west (xmas-path xmas-directions :north-west location)})
+
+(defn find-all [target grid]
   (reduce
    (fn [state [row-idx row]]
      (let [matches (keep-indexed
                     (fn [col-idx letter]
-                      (when (= letter \X) [row-idx col-idx]))
+                      (when (= letter target) [row-idx col-idx]))
                     row)]
        (concat state matches)))
    []
@@ -64,20 +79,46 @@ MXMXAXMASX")
 (defn walk [grid path]
   (map (fn [location] (get-in grid location)) path))
 
-(defn xmas? [result]
+(defn mas? [result]
   (= result [\M \A \S]))
 
 (defn count-surrounding-mas [grid location]
-  (let [ds (keys directions)
-        result (map (fn [d] [d (walk grid (path d location))]) ds)]
-    (count (filter (comp xmas? second) result))))
+  (let [ds (keys mas-directions)
+        result (map (fn [d] [d (walk grid (path mas-directions d location))]) ds)]
+    (count (filter (comp mas? second) result))))
+
+(defn x-mas? [grid location]
+  (let [{:keys [north-east south-east south-west north-west]} (xmas-surrounding location)
+        ne (get-in grid north-east)
+        se (get-in grid south-east)
+        sw (get-in grid south-west)
+        nw (get-in grid north-west)]
+    (and
+     (or (and (= nw \M)
+              (= se \S))
+         (and (= nw \S)
+              (= se \M)))
+     (or (and (= ne \M)
+              (= sw \S))
+         (and (= ne \S)
+              (= sw \M))))))
+
+(defn xmas-locations [grid locations]
+  (map (fn [location] [location (x-mas? grid location)]) locations))
 
 (defn solve [raw]
   (let [data (parse raw)
-        x-locations (find-all-x data)
+        x-locations (find-all \X data)
         results (map (partial count-surrounding-mas data) x-locations)]
     (reduce + 0 results)))
 
+(defn solve2 [raw]
+  (let [data (parse raw)
+        a-locations (find-all \A data)]
+    (count (filter (fn [[_ xmas]] xmas) (xmas-locations data a-locations)))))
+
 (comment
   (solve sample)
-  (solve input))
+  (solve input)
+  (solve2 sample)
+  (solve2 input))
--- advent-of-code/test/aoc2024/04_test.clj
+++ advent-of-code/test/aoc2024/04_test.clj
@@ -1,38 +1,71 @@
 (ns aoc2024.04-test
   (:require
-   [aoc2024.04 :refer [parse directions find-all-x sample surrounding xmas? count-surrounding-mas]]
+   [aoc2024.04 :refer [parse
+                       mas-directions
+                       find-all
+                       sample
+                       mas-surrounding
+                       mas?
+                       count-surrounding-mas]]
    [clojure.test :refer [deftest is testing]]))
 
 (deftest surrounding-test
-  (is (= directions (surrounding [0 0]))))
+  (is (= mas-directions (mas-surrounding [0 0]))))
 
 (deftest find-all-x-test
-  (is (= [[0 4]
-          [0 5]
-          [1 4]
-          [2 2]
-          [2 4]
-          [3 9]
-          [4 0]
-          [4 6]
-          [5 0]
-          [5 1]
-          [5 5]
-          [5 6]
-          [6 7]
-          [7 2]
-          [8 5]
-          [9 1]
-          [9 3]
-          [9 5]
-          [9 9]]
-         (find-all-x (parse sample)))))
+  (testing "find x"
+    (is (= [[0 4]
+            [0 5]
+            [1 4]
+            [2 2]
+            [2 4]
+            [3 9]
+            [4 0]
+            [4 6]
+            [5 0]
+            [5 1]
+            [5 5]
+            [5 6]
+            [6 7]
+            [7 2]
+            [8 5]
+            [9 1]
+            [9 3]
+            [9 5]
+            [9 9]]
+           (find-all \X (parse sample)))))
+  (testing "find a"
+    (is (= [[0 7]
+            [1 2]
+            [1 9]
+            [2 0]
+            [2 6]
+            [2 7]
+            [3 2]
+            [3 4]
+            [4 2]
+            [4 4]
+            [4 7]
+            [5 2]
+            [5 7]
+            [5 9]
+            [6 5]
+            [7 1]
+            [7 3]
+            [7 5]
+            [7 7]
+            [7 8]
+            [7 9]
+            [8 1]
+            [9 4]
+            [9 7]]
+           (find-all \A (parse sample))))))
 
 (deftest xmas?-test
   (is (= false
-         (xmas? [\S \A \M])))
+         (mas? [\S \A \M])))
   (is (= true
-         (xmas? [\M \A \S]))))
+         (mas? [\M \A \S]))))
 
 (deftest count-surrounding-mas-test
   (testing "Seventh X with 0"
