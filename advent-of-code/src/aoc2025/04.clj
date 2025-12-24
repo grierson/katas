df5ec526a96d9afecd322338873f1e48ee28c34b
Kyle Grierson <griersonky@gmail.com>
2025-12-24 16:28:34 +0000

    2025 - day 4 - part 1

---
+++ advent-of-code/src/aoc2025/04.clj
@@ -0,0 +1,48 @@
+(ns aoc2025.04
+  (:require
+   [aoc2023.03 :as aoc2023]
+   [clojure.java.io :as io]
+   [clojure.string :as s]))
+
+(def sample-data
+  "..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.")
+
+(def data (slurp (io/resource "aoc2025/04.txt")))
+
+(defn get-surroundings [grid location]
+  (let [surroundings (aoc2023/surround-locations location)]
+    (remove nil? (map (fn [surrounding] (get-in grid surrounding)) surroundings))))
+
+(defn roll? [grid location]
+  (let [item (get-in grid location)]
+    (= item \@)))
+
+(defn valid-location [grid location]
+  (when (roll? grid location)
+    (let [surroundings (get-surroundings grid location)
+          rolls (filter (fn [item] (= \@ item)) surroundings)]
+      (when (< (count rolls) 4) location))))
+
+(defn solve1 [data]
+  (let [grid (s/split-lines data)]
+    (->> grid
+         (map-indexed
+          (fn [x line]
+            (map-indexed
+             (fn [y _item] (valid-location grid [x y]))
+             line)))
+         (apply concat)
+         (remove nil?)
+         count)))
+
+(comment
+  (solve1 sample-data))
