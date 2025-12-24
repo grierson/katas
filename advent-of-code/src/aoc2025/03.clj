362613d318bb11ee892846f0a292deaee03161ba
Kyle Grierson <griersonky@gmail.com>
2025-12-24 01:12:53 +0000

    2025 - day 3 - just drop the items

--- advent-of-code/src/aoc2025/03.clj
+++ advent-of-code/src/aoc2025/03.clj
@@ -21,43 +21,27 @@
   [coll number]
   (keep-indexed (fn [idx v] (when (= v number) idx)) coll))
 
-(comment
-  (find-indexes (bank->coll "987654321111111") 1))
-
-(defn indexed-bank
-  "{number [indexes]}"
-  [coll]
-  (into {} (map (fn [step] {step (find-indexes coll step)}) (range 1 10))))
-
-(defn find-units
-  "Find unit that comes after tens index"
-  [indexed-bank tens-index]
-  (let [possible-units (filter (fn [[_ v]] (some (fn [idx] (> idx tens-index)) v)) indexed-bank)]
-    (if (empty? possible-units)
-      nil
-      (apply max (keys (into {} possible-units))))))
-
 (defn find-joltage
-  ([indexed-bank] (find-joltage indexed-bank 9))
-  ([indexed-bank tens]
-   (let [tens-indexes (get indexed-bank tens)]
+  ([coll] (find-joltage coll 9))
+  ([coll tens]
+   (let [tens-indexes (find-indexes coll tens)]
      (if (empty? tens-indexes)
-       (find-joltage indexed-bank (dec tens))
-       (if-let [unit (some (fn [tens-index] (when-let [unit (find-units indexed-bank tens-index)] unit)) tens-indexes)]
-         (parse-long (str tens unit))
-         (find-joltage indexed-bank (dec tens)))))))
+       (find-joltage coll (dec tens))
+       (when-let [units (drop (inc (first tens-indexes)) coll)]
+         (if (empty? units)
+           (find-joltage coll (dec tens))
+           (parse-long (str tens (apply max units)))))))))
 
 (comment
   ((requiring-resolve 'hashp.install/install!))
-  (find-joltage (indexed-bank (bank->coll "987654321111111")))
-  (find-joltage (indexed-bank (bank->coll "811111111111119")))
-  (find-joltage (indexed-bank (bank->coll "234234234234278")))
-  (find-joltage (indexed-bank (bank->coll "818181911112111"))))
+  (find-joltage (bank->coll "987654321111111"))
+  (find-joltage (bank->coll "811111111111119"))
+  (find-joltage (bank->coll "234234234234278"))
+  (find-joltage (bank->coll "818181911112111")))
 
 (defn solve1 [banks]
   (let [banks (map bank->coll (str/split-lines banks))]
-    (reduce + 0
-            (map (fn [bank] (find-joltage (indexed-bank bank))) banks))))
+    (reduce + 0 (map find-joltage banks))))
 
 (comment
   ((requiring-resolve 'hashp.install/install!))
